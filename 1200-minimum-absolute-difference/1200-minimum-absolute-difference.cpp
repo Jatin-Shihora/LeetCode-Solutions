65caf035da0160192c9600df4c902c0b4333b5f4
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2021-12-20 08:22:46 +0530

    Time: 52 ms (99.36%), Space: 33.1 MB (22.17%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> minimumAbsDifference(vector<int>& arr) 
-    {
-        sort(arr.begin(), arr.end());
-        pair<int, vector<vector<int>>> result(INT_MAX, {});
-        for(int i = 0; i < arr.size() - 1; ++i)
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        //sort the array using built in method which gives us a TC of O(nlogn)
+        sort(arr.begin(),arr.end());
+        //A pair which has difference and a vector of of two points which stores the min absolute difference
+        pair<int,vector<vector<int>>> result(INT_MAX,{});
+        //Traverse the given array takes O(n) time
+        for(int i=0;i<arr.size()-1;++i) 
         {
-            if(arr[i + 1] - arr[i] > result.first)
-                continue;
-            if(arr[i + 1] - arr[i] == result.first)
-                result.second.push_back({arr[i], arr[i + 1]});
-            else result = {arr[i + 1] - arr[i], {{arr[i], arr[i + 1]}}};
+            //if its not a min absolute difference then use continue and jump to next iteration
+            if(arr[i+1]-arr[i]>result.first) continue;
+            //min diff == diff then add the pair
+            if(arr[i+1]-arr[i]==result.first) result.second.push_back({arr[i],arr[i+1]});
+            //else diff<min diff 
+            else result={arr[i+1]-arr[i],{{arr[i],arr[i+1]}}};    
         }
         return result.second;
     }
