c1fb2aa8045f52c0ffb00d6d4e6ca7d155ca2f72
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2021-12-21 22:55:28 +0530

    Time: 0 ms (100.00%), Space: 6 MB (22.94%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<=0) return false;
-        return ((n&(n-1))==0);
+        if(n <= 0) return false;
+        return ceil(log2(n)) == floor(log2(n));
     }
 };
