5b5cfe6fa1b404618d8da2625ad731eaab9172db
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2021-12-22 09:16:25 +0530

    Time: 36 ms (83.28%), Space: 17.7 MB (76.71%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -11,21 +11,39 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        //base case i.e if the linked list has zero,one or two elments just return it
-        if(!head || !head->next || !head->next->next) return;
+        // base case : linkedlist is empty
+        if (!head) return;
         
-        //Find the penultimate node i.e second last node of the linkedlist
-        ListNode* penultimate = head;
-        while (penultimate->next->next) penultimate = penultimate->next;
+        // finding the middle with the help of two pointer approach
+        ListNode *tmp = head, *half = head, *prev = NULL;
+        while (tmp->next && tmp->next->next) {
+            tmp = tmp->next->next;
+            half = half->next;
+        }
         
-        // Link the penultimate with the second element
-        penultimate->next->next = head->next;
-        head->next = penultimate->next;
+        // adding one bit in case of lists with even length
+        if (tmp->next) half = half->next;
         
-        //Again set the penultimate to the the last 
-        penultimate->next = NULL;
+        // Now reverse the second half
+        while (half) {
+            tmp = half->next;
+            half->next = prev;
+            prev = half;
+            half = tmp;
+        }
+        half = prev;
         
-        // Do the above steps recursive
-        reorderList(head->next->next);
+        // After reversing the second half, let's merge both the halfes
+        while (head && half) {
+            tmp = head->next;
+            prev = half->next;
+            head->next = half;
+            half->next = tmp;
+            head = tmp;
+            half = prev;
+        }
+        
+        // Base case : closing when we had even length arrays
+        if (head && head->next) head->next->next = NULL;
     }
 };
