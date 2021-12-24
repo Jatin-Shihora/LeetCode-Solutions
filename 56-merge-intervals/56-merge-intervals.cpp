1580ba45824e14a2f62d0713734f95bc22f106e8
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2021-12-24 08:17:02 +0530

    Time: 16 ms (88.84%), Space: 14.2 MB (55.12%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& interval) {
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        //base case
+        if(intervals.size()<=1) return intervals;
+        //resultan vector
         vector<vector<int>> result;
-        //base case when threr are no intervals
-        if(interval.size()==0)return result;
         //sort takes O(nlogn) time
-        sort(interval.begin(),interval.end());
+        sort(intervals.begin(), intervals.end()); 
         //insert the the the first interval in the result
-        result.push_back(interval[0]);
-        int j=0;
-        //Treverse the whole vector .Takes O(n) time
-        for(int i=1;i<interval.size();i++)
-        {
-            //if intevals are overlapping
-            if(result[j][1]>=interval[i][0])
-                result[j][1]=max(result[j][1],interval[i][1]);
+        result.push_back(intervals[0]);
+         //Traverse the whole vector .Takes O(n) time
+        for(int i=1; i<intervals.size(); i++) {
+             //if intevals are overlapping
+            if(result.back()[1] >= intervals[i][0]) result.back()[1] = max(result.back()[1] , intervals[i][1]);
             //else they are not overlapping
-            else
-            {
-                j++;
-                result.push_back(interval[i]);
-            }
+            else result.push_back(intervals[i]); 
         }
         return result;
     }
