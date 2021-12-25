57e1129a4004d21ac0623cd2981702f7835b5aa2
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2021-12-25 09:30:40 +0530

    Time: 8 ms (87.98%), Space: 7.7 MB (96.70%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+ int calculate(string s) {
+    long long sum = 0; 
+    if(s.size() < 1) return sum; 
+    long long i = 0; 
+    long long last = 0, last_result = 1;  
+    char last_operator = '+'; //remember the last operator
+    long sign = 1; 
+    while(i < s.size()){
+        if(s[i] == ' '){++i; continue;}
+        if(s[i] == '+' || s[i] == '-'){
+            sum += last_result * sign;
+            sign = s[i++] == '+' ? 1 : -1; 
+            last_result = 1;
+            last_operator = '+'; 
+        }
+        else if(s[i] == '/' || s[i] == '*'){
+            last_operator = s[i];
+            ++i; 
+        }
+        if(isdigit(s[i])){
+            last = 0; 
+            while(i < s.size() && isdigit(s[i])){
+                last =  last * 10 + s[i] - '0'; 
+                i++;
+            }
+            
+            if(last_operator == '*') last_result *= last;
+            else if(last_operator == '/') last_result /= last;
+            else  last_result = last; 
+        }
+    }
+    sum += last_result * sign;
+    return sum; 
+}
+};
