d99ad5482ba8c15b6216cdce012cde287055d86c
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2021-12-27 08:41:12 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (6.38%) - LeetHub

--- 476-number-complement/476-number-complement.cpp
+++ 476-number-complement/476-number-complement.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     int findComplement(int num) {
-        unsigned mask = ~0;
-        while( mask & num ) mask = mask << 1;
-        return ~num ^ mask;
+        vector<int> temp; 
+		// convert to binary representation
+        while( num != 0 ){
+            temp.push_back( num % 2 );
+            num /= 2;
+        } 
+		// make complement
+        for(int i=0; i<temp.size(); i++){
+            if( temp[i] == 1 ) temp[i] = 0;
+            else if( temp[i] == 0 ) temp[i] = 1;
+        } 
+        int res = 0;
+        for(int i=temp.size()-1; i>-1; i--) res = res * 2 + temp[i];
+        return res;
     }
 };
