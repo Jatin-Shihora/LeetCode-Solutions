b9619d4b0a49ddfd40a770c9a6f87acdd5b9bc13
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2021-12-31 08:34:38 +0530

    Time: 4 ms (81.69%), Space: 9.8 MB (74.27%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -12,11 +12,6 @@
 class Solution {
 public:
     int maxAncestorDiff(TreeNode* root, int mini = INT_MAX, int maxi = INT_MIN) {
-        // base case
-        if (!root) return (maxi - mini);
-        // update mini and maxi
-        mini = min(mini, root->val), maxi = max(maxi, root->val);
-        // finding the maximum distance we would like to find by going left and right
-        return max(maxAncestorDiff(root->left, mini, maxi), maxAncestorDiff(root->right, mini, maxi));
+        return root ? max(maxAncestorDiff(root->left, mini = min(mini, root->val), maxi = max(maxi, root->val)), maxAncestorDiff(root->right, mini, maxi)) : maxi - mini;
     }
 };
