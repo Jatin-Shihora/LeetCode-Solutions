ecdc47c7741a4facdd61d3c32c03a0085409d359
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-01-04 08:01:19 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.09%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     int bitwiseComplement(int num) {
-        //base case
-        if(num==0) return 1;
-        vector<int> temp; 
-		// convert to binary representation
-        while( num != 0 ){
-            temp.push_back( num % 2 );
-            num /= 2;
-        } 
-		// make complement
-        for(int i=0; i<temp.size(); i++){
-            if( temp[i] == 1 ) temp[i] = 0;
-            else if( temp[i] == 0 ) temp[i] = 1;
-        } int res = 0;
-        for(int i=temp.size()-1; i>-1; i--) res = res * 2 + temp[i];
-        return res;
+		    //base case
+        if(num == 0) return 1;
+        unsigned mask = ~0;
+        while( mask & num ) mask = mask << 1;
+        return ~num ^ mask;
     }
 };
