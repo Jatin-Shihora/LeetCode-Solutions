6202a210db4bba76d12f6fbb5b307b01e8f55932
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-01-08 09:45:09 +0530

    Time: 87 ms (32.86%), Space: 14.6 MB (17.14%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
+        //m=number of rows and n = number of columns
         int m = (int)grid.size(), n = (grid[0].size());
+        //initializing m and n
         vector<vector<vector<int>>> dp(m + 1, vector<vector<int>>(n, vector<int>(n, 0)));
+        //dp dimensions
         dp[0][0][n - 1] = 0 == n - 1 ? grid[0][0] : grid[0][0] + grid[0][n - 1];
+        //row traversing
         for (int row = 1; row < m; row++) {
+            //avoiding unnecessary column computations
             for (int c1 = 0; c1 < min(n, row + 1); c1++) {
                 for (int c2 = max(0, n - 1 - row); c2 < n; c2++) {
-                    int prev_max = 0;
+                    int prevMax = 0;
+                    //Moves of robos in columns
                     for (int offset1 = max(0, c1 - 1); offset1 <= min(n - 1, c1 + 1); offset1++) {
                         for (int offset2 = max(0, c2 - 1); offset2 <= min(n - 1, c2 + 1); offset2++) {
-                                prev_max = max(prev_max, dp[row - 1][offset1][offset2]);
+                                prevMax = max(prevMax, dp[row - 1][offset1][offset2]); //9 states wediscussed can be summariazed 
                         }
                     }
-                    if (c1 == c2)
-                        dp[row][c1][c2] = prev_max + grid[row][c1];
-                    else
-                        dp[row][c1][c2] = prev_max + grid[row][c1] + grid[row][c2];
+                    //Robo collision condition
+                    if (c1 == c2) dp[row][c1][c2] = prevMax + grid[row][c1];
+                    else dp[row][c1][c2] = prevMax + grid[row][c1] + grid[row][c2];
                 }
             }
         }
