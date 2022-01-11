156cc0a924fd2c82e40ea72775acdbe5aaa3a4d6
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-01-11 08:38:22 +0530

    Time: 4 ms (87.29%), Space: 16.7 MB (43.41%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -13,7 +13,9 @@ class Solution {
 public:
     int sumRootToLeaf(TreeNode* root, int val = 0) {
         if (!root) return 0;//base condition
-        val = val << 1 | root->val;
+        //Both are same it's just the bit manipulation way  
+        //val = val << 1 | root->val;
+        val = (val * 2 + root->val);
         return (root->left == root->right ? val : sumRootToLeaf(root->left, val) + sumRootToLeaf(root->right, val));
     }
 };
