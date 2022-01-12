d4aaef8f2eea446e7f790a73055f0386ef9a8c2d
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-01-12 08:07:31 +0530

    Time: 133 ms (22.93%), Space: 56.9 MB (80.92%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -9,12 +9,18 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
+Time Complexity :- O(H), where H is height of tree
+Space Complexity :- O(H) as considering recursion stack, takes place in internal memory, if not consider then O(1)
+*/
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
         if(!root) return new TreeNode(val); //if the bst tree is empty create a new node for the value
-        if(val > root->val) root->right = insertIntoBST(root->right,val) ;
-        if(val < root->val) root->left = insertIntoBST(root->left, val);
+        //The value which is higher than the parent node are always on the right.
+        if(val > root->val) root->right = insertIntoBST(root->right,val) ; 
+        //The value which is lower than the parent node are always on the left.        
+        else root->left = insertIntoBST(root->left,val);
         return root;
     }
 };
