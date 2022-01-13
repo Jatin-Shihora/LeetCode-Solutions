94bea85ff139f4e0a5260c208c2ad6cf7d9fce74
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-01-13 08:37:04 +0530

    Time: 428 ms (60.32%), Space: 89.8 MB (63.52%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,23 +1,20 @@
 class Solution {
+    static bool compare(vector<int> &v1, vector<int> &v2){ return v1[1] < v2[1]; }
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-        if (points.empty()) return 0;//base case if there are no balloons
-        //Sort the array by end points i.e p[][endpoint]
-        sort(points.begin(), points.end(), [] (auto &p1, auto&p2) {
-            return p1[1] < p2[1];
-        });
-        //end point
-        int end = points[0][1];
-		
-		// the reason res = 1 is that we need an arrow to destroy the last group. 
-        int res = 1;
-        //Traverse through all points 
-        for (auto point: points)
-        {
-            if (point[0] <= end) continue;
-            res++;
-            end = point[1];
+        int n=points.size();
+        if(n <= 1) return n;
+        sort(points.begin(), points.end(), compare);
+        
+        int ans=1, prev=points[0][1];
+        
+        for(int i=1; i<n; i++){
+            if(points[i][0] <= prev) continue;
+            
+            ans++;
+            prev = points[i][1];
         }
-        return res;
+        
+        return ans;
     }
 };
