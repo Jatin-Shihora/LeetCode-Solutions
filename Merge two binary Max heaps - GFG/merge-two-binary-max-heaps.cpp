8d7555853b4002782a3765a32616967a5e1c16ce
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-01-15 09:07:19 +0530

    Added solution - LeetHub

---
+++ Merge two binary Max heaps - GFG/merge-two-binary-max-heaps.cpp
@@ -0,0 +1,82 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+// C++ program to merge two max heaps.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    private:
+    void max_heapify(vector<int>& heap) {
+        int i = heap.size()-1;
+        while(i != 0) {
+            int pi = (i-1)/2;
+            if(heap[pi] < heap[i]) {
+                int t = heap[pi];
+                heap[pi] = heap[i];
+                heap[i] = t;
+            }
+            else break;
+            i = pi;
+        }
+    }
+    
+    public:
+    vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
+        //your code
+        for(int i=0; i<m; i++) {
+            a.push_back(b[i]);
+            max_heapify(a);
+        }
+        
+        return a;
+    }
+};
+
+// { Driver Code Starts.
+
+bool isMerged(vector<int> &arr1, vector<int> &arr2, vector<int> &merged){
+    if (arr1.size() + arr2.size() != merged.size()){
+        return false;
+    }
+    arr1.insert(arr1.end(),arr2.begin(),arr2.end());
+    sort(arr1.begin(),arr1.end());
+    vector<int> mergedCopy =merged;
+    sort(mergedCopy.begin(),mergedCopy.end());
+    if (arr1 != mergedCopy){
+        return false;
+    }
+    for(int i = 1; i<merged.size(); i++){
+        if(merged[i] > merged[(i-1)/2])
+            return false;
+    }
+    return true;
+}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, i;
+        cin >> n >> m;
+        vector<int> a(n,0),b(m,0);
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        for (i = 0; i < m; i++) {
+            cin >> b[i];
+        }
+        vector<int> merged, copyA = a, copyB = b;
+        Solution obj;
+        merged = obj.mergeHeaps(a, b, n, m);
+        bool flag = isMerged(copyA, copyB, merged);
+        if(flag == false) cout<<0<<endl;
+        else cout<<1<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
