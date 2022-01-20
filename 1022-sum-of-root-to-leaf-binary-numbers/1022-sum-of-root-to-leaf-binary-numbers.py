daf722bdc2d2bc58918bcbccafa87241ad3bd9c2
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-01-20 09:30:40 +0530

    Time: 41 ms (34.65%), Space: 14 MB (56.77%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -8,6 +8,6 @@ class Solution(object):
     def sumRootToLeaf(self, root, val=0):
         if not root: return 0  #base case
         val = val * 2 + root.val  #val = val << 1 | root->val; //Both are same it's just the bit manipulation way  
-        if root.left == root.right: return val 
+        if not root.left and not root.right: return val 
         return self.sumRootToLeaf(root.left, val) + self.sumRootToLeaf(root.right, val) #dfs call
         
