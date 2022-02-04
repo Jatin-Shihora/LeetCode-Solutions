59edb0e76629c991edd6a97c2e2f08b448e3f85b
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-02-04 10:31:06 +0530

    Time: 744 ms (11.62%), Space: 10.2 MB (66.51%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    //Two pass , TC=O(n^2) and SC = O(n)
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i=0;i<nums.size()-1 ; i++){
+            for(int j =i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+            }
+        }
+        return ans;
+    }
+};
