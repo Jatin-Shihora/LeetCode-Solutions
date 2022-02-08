f12a6cf7d4174f6f732f86faad410cd671a5f211
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-02-08 08:08:27 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,20 +1,8 @@
 class Solution {
 public:
-    /*
-    I have 2 loops: the external runs until num > 9, ie: I have 2 digits.
-Inside the inner one, I take one digit at a time and add it to tmp, 
-reducing num proportionally; once the inner loop is done, 
-I assign num the new value.
-    */
     int addDigits(int num) {
-        int tmp;
         while(num>9){
-            tmp = 0;
-            while(num){
-                tmp+=num%10;
-                num/=10;
-            }
-            num = tmp;
+            num=num/10 + num%10;
         }
         return num;
     }
