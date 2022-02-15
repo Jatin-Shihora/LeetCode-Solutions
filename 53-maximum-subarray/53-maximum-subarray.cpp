47f8378cdc0339ff63aad9926decbafcc7219bd0
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-02-15 17:33:10 +0530

    Time: 158 ms (44.84%), Space: 67.7 MB (54.35%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0, maxi = INT_MIN;
+        for(auto it: nums){
+            sum += it;
+            maxi = max(sum,maxi);
+            if(sum<0) sum = 0;
+        }
+        return maxi;
+    }
+};
