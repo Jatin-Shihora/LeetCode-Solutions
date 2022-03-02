54713f3517a968802e327513524c861994861261
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-03-02 16:57:28 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (90.23%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,17 @@
+// Approach Using TWO POINTER
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        int i = 0, j = 0;
+        while(i < m && j < n) {
+            if(s[i] == t[j]) {
+                i++;
+            }
+            j++;
+        }
+        return i == m ? 1 : 0;
+    }
+};
