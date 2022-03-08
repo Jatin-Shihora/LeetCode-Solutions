6cc116270b67efd16439bdc731ac0fed09367767
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-03-08 17:46:33 +0530

    Time: 12 ms (49.02%), Space: 14.8 MB (45.62%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,16 +10,52 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1==NULL) return l2;
-        if(l2==NULL) return l1;
-        if(l1->val < l2->val){
-            l1->next = mergeTwoLists(l1->next,l2);
-            return l1;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+       
+	    // if list1 happen to be NULL
+		// we will simply return list2.
+        if(list1 == NULL)
+            return list2;
+		
+		// if list2 happen to be NULL
+		// we will simply return list1.
+        if(list2 == NULL)
+            return list1;
+        
+        ListNode * ptr = list1;
+        if(list1 -> val > list2 -> val)
+        {
+            ptr = list2;
+            list2 = list2 -> next;
         }
-        else{
-            l2->next = mergeTwoLists(l1,l2->next);
-            return l2;
+        else
+        {
+            list1 = list1 -> next;
         }
+        ListNode *curr = ptr;
+        
+		// till one of the list doesn't reaches NULL
+        while(list1 &&  list2)
+        {
+            if(list1 -> val < list2 -> val){
+                curr->next = list1;
+                list1 = list1 -> next;
+            }
+            else{
+                curr->next = list2;
+                list2 = list2 -> next;
+            }
+            curr = curr -> next;
+                
+        }
+		
+		// adding remaining elements of bigger list.
+        if(!list1)
+            curr -> next = list2;
+        else
+            curr -> next = list1;
+            
+        return ptr;
+       
     }
 };
