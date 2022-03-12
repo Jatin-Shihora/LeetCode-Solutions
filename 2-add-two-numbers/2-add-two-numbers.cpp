8482af8743dbe9a08e8a58d7481829ef5de9b022
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-03-12 17:09:27 +0530

    Time: 37 ms (75.04%), Space: 71.4 MB (81.33%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -12,14 +12,26 @@ class Solution {
     //Recursive
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-    if(!l1 && !l2) return NULL;
-    int c = (l1? l1->val:0) + (l2? l2->val:0);
-    ListNode *newHead = new ListNode(c%10), *next = l1? l1->next:NULL;
-    c /= 10;
-    if(next) next->val += c;
-    else if(c) next = new ListNode(c);
-    newHead->next = addTwoNumbers(l2? l2->next:NULL, next);
-    return newHead;
-}
-
+        ListNode *dummy = new ListNode();
+        ListNode *temp = dummy;
+        int carry=0;
+        while(l1!=NULL || l2!=NULL || carry){
+            int sum = 0; 
+            if(l1!=NULL){
+                sum += l1->val;
+                l1=l1->next;
+            }
+            if(l2!=NULL){
+                sum += l2->val;
+                l2=l2->next;
+            }
+            
+            sum+=carry;
+            carry = sum/10;
+            ListNode* node = new ListNode(sum%10);
+            temp->next = node;
+            temp= temp->next;
+        }
+        return dummy->next;
+    }
 };
