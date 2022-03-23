7c64517eaeb4045c3738ed988cc27b4f12bc5728
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-03-23 19:34:18 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (30.21%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        int res = 0;
+        while (Y > X) {
+            Y = Y % 2 > 0 ? Y + 1 : Y / 2;
+            res++;
+        }
+        return res + X - Y;
+    }
+};
