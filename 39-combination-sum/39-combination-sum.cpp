e2926e644065b09d1fd26e596011664beae75edb
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-04-02 19:06:42 +0530

    Time: 3 ms (96.42%), Space: 10.7 MB (88.61%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>>result;
-vector<int>current;
-
-void function(vector<int>& candidates,int target,int index)
-{
-    if(target==0){
-        result.push_back(current);
-        return;
+    void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds){
+        //base case
+        if(ind == arr.size()){
+            if(target==0){
+                ans.push_back(ds);
+            }
+            return ;
+        }
+        //pick up element condition 
+        if(arr[ind] <=target){
+            ds.push_back(arr[ind]);
+            findCombination(ind,target-arr[ind],arr,ans,ds);
+            ds.pop_back();
+        }
+        findCombination(ind+1,target,arr,ans,ds);
     }
     
-    if(index==candidates.size() || target<0)return;
-    
-    current.push_back(candidates[index]);
-    function(candidates,target-candidates[index],index);
-    current.pop_back();
-    function(candidates,target,index+1);      
-}
-
-vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-    function(candidates,target,0);
-    return result;
-}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findCombination(0,target,candidates,ans,ds);
+        return ans;
+    }
 };
