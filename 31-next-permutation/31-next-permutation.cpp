b189fc2c37071ac4295160a1973a1f3cb9f3d9a0
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-04-03 10:31:28 +0530

    Time: 8 ms (38.40%), Space: 12.1 MB (75.54%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size(), k , l;// K is the break point and l is the second index we talked 
-        for(k=n-2;k>=0;k--) {
+        int n = nums.size(),k , l ;
+        //k=break point 
+        //Finding the break point
+        for(k=n-2;k>=0;k--){
             if(nums[k]<nums[k+1]) break;
         }
         if(k<0) {
-            reverse(nums.begin(), nums.end());
+            reverse(nums.begin(),nums.end());
             return;
         }
-        else {
+        //finding the just greater element than k  
+        else{
             for(l=n-1;l>k;l--){
-                if(nums[l] > nums[k]) break;
+                if(nums[l]>nums[k]) break;
             }
         }
+        //swap those above both index
         swap(nums[k],nums[l]);
-        reverse(nums.begin()+ k+1, nums.end());
+        //Now reverse from the index of l
+        reverse(nums.begin()+k+1, nums.end());
     }
 };
