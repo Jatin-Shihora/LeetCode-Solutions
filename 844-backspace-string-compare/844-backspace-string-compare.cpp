47a0540ee8b53c4844a4cc812a722b982094e630
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-05-01 18:06:51 +0530

    Time: 4 ms (28.12%), Space: 6.3 MB (68.92%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int k=0,p=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='#')
+            {
+                k--;
+                 k=max(0,k);
+            }
+            
+           else
+           {
+               s[k]=s[i];
+               k++;
+           }
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            if(t[i]=='#')
+            {
+                p--;
+                 p=max(0,p);
+            }
+            
+           else
+           {
+               t[p]=t[i];
+               p++;
+           }
+        }
+        if(k!=p)
+            return false;
+        else
+        {
+            for(int i=0;i<k;i++)
+            {
+                if(s[i]!=t[i])
+                    return false;
+            }
+            return true;
+        }
+        
+    }
+};
