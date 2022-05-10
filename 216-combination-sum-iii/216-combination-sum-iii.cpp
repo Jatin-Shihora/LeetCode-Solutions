b43fecfea141041f8dee1e0f7079299f610327b3
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-05-10 23:23:17 +0530

    Time: 5 ms (23.39%), Space: 6.7 MB (37.26%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+  void combination(vector<vector<int>>& result, vector<int> sol, int k, int n) {
+    if (sol.size() == k && n == 0) { result.push_back(sol); return ; }
+    if (sol.size() < k) {
+      for (int i = sol.empty() ? 1 : sol.back() + 1; i <= 9; ++i) {
+        if (n - i < 0) break;
+        sol.push_back(i);
+        combination(result, sol, k, n - i);
+        sol.pop_back();
+      }
+    }
+  }
+
+  vector<vector<int>> combinationSum3(int k, int n) {
+    vector<vector<int>> result;
+    vector<int> sol;
+    combination(result, sol, k, n);
+    return result;
+  }
+};
