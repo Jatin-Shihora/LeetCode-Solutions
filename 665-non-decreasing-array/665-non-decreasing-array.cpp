55dfd67714ffd928f4fda9c993a0c12678e9286b
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-06-25 09:41:40 +0530

    Time: 51 ms (24.65%), Space: 27 MB (81.98%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+bool checkPossibility(vector<int>& nums) {
+        int cnt = 0;                                                                    //the number of changes
+        for(int i = 1; i < nums.size() && cnt<=1 ; i++){
+            if(nums[i-1] > nums[i]){
+                cnt++;
+                if(i-2<0 || nums[i-2] <= nums[i])nums[i-1] = nums[i];                    //modify nums[i-1] of a priority
+                else nums[i] = nums[i-1];                                                //have to modify nums[i]
+            }
+        }
+        return cnt<=1;
+    } 
+};
