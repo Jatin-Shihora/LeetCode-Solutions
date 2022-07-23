1c3a6a5966efd0442eb4171a4f3922bc4404d25e
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-07-23 19:36:24 +0530

    Time: 833 ms (50.08%), Space: 209.3 MB (42.97%) - LeetHub

---
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    void merge(vector<int> &count, vector<pair<int, int> > &v, int left, int mid, int right) {
+        vector<pair<int, int> > tmp(right-left+1);
+        int i = left;
+        int j = mid+1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+            // mind that we're sorting in descending order
+            if (v[i].first <= v[j].first) { 
+                tmp[k++] = v[j++];
+            }
+            else {
+                // only line responsible to update count, related to problem constraint, 
+                // remaining part is just regular mergeSort 
+                count[v[i].second] += right - j + 1;
+                tmp[k++] = v[i++];
+            }
+        }
+        while (i <= mid) {
+            tmp[k++] = v[i++];
+        }
+        while (j <= right) {
+            tmp[k++] = v[j++];
+        }
+        for (int i = left; i <= right; i++)
+            v[i] = tmp[i-left];
+    }        
+
+    void mergeSort(vector<int> &count, vector<pair<int, int> > &v, int left, int right) {
+        if (left >= right) 
+            return;
+
+        int mid = left + (right-left)/2;
+        mergeSort(count, v, left, mid);
+        mergeSort(count, v, mid+1, right);
+        merge(count, v, left, mid, right);
+    }
+
+    vector<int> countSmaller(vector<int>& nums) {
+        int N = nums.size();
+
+        vector<pair<int, int> > v(N);
+        for (int i = 0; i < N; i++)   
+            v[i] = make_pair(nums[i], i);
+
+        vector<int> count(N, 0);
+        // sorting in descending order
+        mergeSort(count, v, 0, N-1);
+
+        return count;
+    }
+};
