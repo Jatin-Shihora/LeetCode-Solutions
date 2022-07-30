efc2120d5e6c69c91e1d16b7ad4281a43e83eb3b
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-07-30 11:28:16 +0530

    Time: 141 ms (74.13%), Space: 67.7 MB (53.82%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    //Kadens Algo
     int maxSubArray(vector<int>& nums) {
-        int sum = 0, maxi = INT_MIN;
-        for(auto it: nums){
-            sum += it;
-            maxi = max(sum,maxi);
-            if(sum<0) sum = 0;
-        }
+        int sum= 0 ,  maxi= nums[0] ;
+            for(int i = 0 ; i<nums.size() ; i++ ){
+                sum = sum + nums[i];
+                maxi= max(sum,maxi);
+                if(sum <0) sum=0 ;
+            }
         return maxi;
     }
 };
