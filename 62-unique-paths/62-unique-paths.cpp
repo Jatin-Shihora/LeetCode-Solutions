45b0f430222abcfebf87fc56e65a3bc38de6ebe6
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-08-02 11:11:50 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (46.25%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n));
-        return dfs(dp, 0, 0);
+    //This is DP solution 
+    int countPaths(int i, int j ,int row, int column,vector<vector<int>> &dp){
+        if(i==(row-1) && j== (column-1)) return 1;
+        
+        if(i>=row || j >= column) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        else return dp[i][j]=countPaths(i+1,j,row,column,dp)+countPaths(i,j+1,row,column,dp);
     }
-    int dfs(vector<vector<int>>& dp, int i, int j) {
-        if(i >= size(dp)   || j >= size(dp[0]))   return 0;     // out of bounds - invalid
-        if(i == size(dp)-1 && j == size(dp[0])-1) return 1;     // reached end - valid path
-        if(dp[i][j]) return dp[i][j];                           // directly return if already calculated
-        return dp[i][j] = dfs(dp, i+1, j) + dfs(dp, i, j+1);    // store the result in dp[i][j] and then return
+    int uniquePaths(int m, int n){
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        
+        int num =  countPaths(0,0,m,n,dp); 
+        if(m==1 && n==1) return num; 
+        return dp[0][0];
     }
-};
+}; 
