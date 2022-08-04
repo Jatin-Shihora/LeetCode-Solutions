ddbf474b756b3c19f000b2cfaff70161b6df1b5b
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-08-04 08:18:18 +0530

    Time: 4 ms (23.97%), Space: 5.9 MB (26.45%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //Dont know how to solve :|
     int mirrorReflection(int p, int q) {
         while (p % 2 == 0 && q % 2 == 0) p >>= 1, q >>= 1;
         return 1 - p % 2 + q % 2;
