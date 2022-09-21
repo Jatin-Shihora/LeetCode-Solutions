e9593a5d8c756d4c54cce6a8115e6472b121183a
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-09-21 20:50:56 +0530

    Time: 99 ms (98.61%), Space: 45.3 MB (97.42%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -1,37 +1,30 @@
 class Solution {
-public:
-    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+  public:
+    vector < int > sumEvenAfterQueries(vector < int > & nums, vector < vector < int >> & queries) {
+      int len_queries = queries.size();
+      int sum = 0;
+      vector < int > ans(len_queries, 0);
+      //calculate the sum for the even elements of the nums
+      for (int & x: nums) {
+        if (!(x & 1))
+          sum += x;
+      }
+      for (int i = 0; i < len_queries; i++) {
+        int val = queries[i][0];
+        int idx = queries[i][1];
 
-        int sum = 0, N = queries.size();
-        for (int n : nums) {
-            if (n % 2 == 0) sum += n;
-        }
-        
-        vector<int> ans(N, 0);
-        
-        // Four cases to handle.
-        for (int i = 0; i < N; i++) {
-            int val = queries[i][0], index = queries[i][1];
-            int oldValue = nums[index];
-            nums[index] += val;
-            
-            bool wasEven = (oldValue % 2) == 0;
-            bool nowEven = (nums[index] % 2 == 0);
-            
-            if (wasEven && nowEven) {
-                sum += val;
-                ans[i] = sum;
-            } else if (!wasEven && nowEven) {
-                sum += nums[index];
-                ans[i] = sum;
-            } else if (wasEven && !nowEven) {
-                sum -= oldValue;
-                ans[i] = sum;
-            } else {
-                ans[i] = sum;
-            }
-        }
-        
-        return ans;
+        //if nums[idx] is even subtract
+        if (!(nums[idx] & 1))
+          sum -= nums[idx];
+
+        nums[idx] += val;
+
+        //if nums[idx] is now even after adding val to it then add it to sum
+        if (!(nums[idx] & 1))
+          sum += nums[idx];
+        //save to the ans vector
+        ans[i] = sum;
+      }
+      return ans;
     }
 };
