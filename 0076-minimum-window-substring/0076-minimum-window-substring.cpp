27582da85adadf267c27ab1e4927f52e8b9c29d9
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-10-22 12:09:37 +0530

    Time: 8 ms (95.68%), Space: 7.8 MB (68.10%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        vector<int> map(128,0);
+        for(auto c: t) map[c]++;
+        int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
+        while(end<s.size()){
+            if(map[s[end++]]-->0) counter--; //in t
+            while(counter==0){ //valid
+                if(end-begin<d)  d=end-(head=begin);
+                if(map[s[begin++]]++==0) counter++;  //make it invalid
+            }  
+        }
+        return d==INT_MAX? "":s.substr(head, d);
+    }
+};
