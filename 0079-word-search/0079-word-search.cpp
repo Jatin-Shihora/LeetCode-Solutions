7b64f0db7f8fbdee28cfd26c5d1113fbd3690682
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-11-24 20:37:24 +0530

    Time: 484 ms (67.14%), Space: 7.9 MB (79.86%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -5,7 +5,7 @@ public:
         //Traverse the matrix
         for(unsigned int i=0;i<row;i++){
             for(unsigned int j=0;j<col;j++){
-                //check if the words exists or not with the help of dfs
+                //if the position on the board corresponds to starting letter, then call the search function
                 if(dfs(board,i,j,word)) return true; 
             }
         }
@@ -16,15 +16,19 @@ public:
         if(!word.size()) return true;
         //if out of bounds i.e left,right,up,down then return false;
         if(i<0 || i>=board.size() || j<0 || j>= board[0].size() || board[i][j]!=word[0]) return false;
-        //put current char to c
+        //get character of present position
         char c = board[i][j];
-        //Once assigned to c put a '*' marke at that index
+        //change it to other character to avoid recursively calling the same node again and again
         board[i][j]='*';
         //copy the word from index 1 till the end [Note:index starts from  0]
         string s = word.substr(1);
-        //dfs => left || right || down || up
+        //if any of the searchers are true, return true  
+        //search from the left position on the board
+        //search from the right position on the board
+        //search from the down position on the board
+        //search from the up position on the board
     bool ret = dfs(board, i-1, j, s) || dfs(board, i+1, j, s) || dfs(board, i, j-1, s) || dfs(board, i, j+1, s);
-        //Reassign c to the '*' marked index 
+        //change the character back to original character
         board[i][j] = c;
         //final ans
         return ret;
