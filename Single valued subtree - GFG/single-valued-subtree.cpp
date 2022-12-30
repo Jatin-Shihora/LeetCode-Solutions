985ccedf05f5c65dadb9779064096f79ef27cba6
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2022-12-30 07:50:30 +0530

    Added solution - LeetHub

--- Single valued subtree - GFG/single-valued-subtree.cpp
+++ Single valued subtree - GFG/single-valued-subtree.cpp
@@ -100,23 +100,44 @@ struct Node
 };*/
 class Solution {
 public:
-    int res;
-    int solve(Node* root) {
-        if(!root) return 1;
-        
-        int ans = 1;
-        if(root->left and root->data != root->left->data) ans = 0;
-        if(root->right and root->data != root->right->data) ans = 0;
-        
-        ans &= solve(root->left) & solve(root->right);
-        res += ans;
-        return ans;
+    bool countSingleRec(Node* root, int &count){
+	// Return false to indicate NULL
+	if (root == NULL) return true;
+
+	// Recursively count in left and right subtrees also
+	bool left = countSingleRec(root->left, count);
+	bool right = countSingleRec(root->right, count);
+
+	// If any of the subtrees is not singly, then this
+	// cannot be singly.
+	if (left == false || right == false) return false;
+
+	// If left subtree is singly and non-empty, but data
+	// doesn't match
+	if (root->left && root->data != root->left->data) return false;
+
+	// Same for right subtree
+	if (root->right && root->data != root->right->data) return false;
+
+	// If none of the above conditions is true, then
+	// tree rooted under root is single valued, increment
+	// count and return true.
+	count++;
+	
+	return true;
     }
-    
-    int singlevalued(Node *root) {
-        res = 0;
-        solve(root);
-        return res;
+
+// This function mainly calls countSingleRec()
+// after initializing count as 0
+public:
+    int singlevalued(Node* root){
+	// Initialize result
+	int count = 0;
+
+	// Recursive function to count
+	countSingleRec(root, count);
+
+	return count;
     }
 };
 
