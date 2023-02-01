c41ecbca52971e789ca3d447d30d66b0225304b4
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-02-01 21:40:07 +0530

    Time: 3 ms (83.39%), Space: 7.3 MB (32.86%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        return (str1 + str2 == str2 + str1)? 
+        str1.substr(0, gcd(size(str1),size(str2))): "";
+    }
+};
