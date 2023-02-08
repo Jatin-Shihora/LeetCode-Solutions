8482759fa0999e7321e01297494791a2ed124479
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-02-08 18:30:23 +0530

    Time: 486 ms (23.18%), Space: 17.2 MB (29.40%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int solve(vector<int> &nums, vector<int> &dp, int pos){
-        if(pos>=nums.size()-1) return 0;
-        if(dp[pos]!=10001) return dp[pos];
-        for(int j=1;j<=nums[pos];j++)
-            dp[pos] =  min(dp[pos],1+solve(nums,dp,pos+j));
-        return dp[pos];
-    }
-    int jump(vector<int>& nums,int pos = 0) {
-        vector<int> dp(nums.size(), 10001);
-        return solve(nums,dp,0);
-    }
+    int jump(vector<int>& nums) {
+	int n = size(nums);
+	vector<int> dp(n, 10001);
+	dp[n - 1] = 0;  // start from last index. No jumps required to reach end if we are already here
+	// same as above. For each index, explore all jump sizes and use the one requiring minimum jumps to reach end
+	for(int i = n - 2; i >= 0; i--) 
+		for(int jumpLen = 1; jumpLen <= nums[i]; jumpLen++) 
+			dp[i] = min(dp[i], 1 + dp[min(n - 1, i + jumpLen)]);  // min(n-1, i + jumpLen) for bounds handling
+	return dp[0];
+}
 };
