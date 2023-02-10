2a2cac50738d06ef447bbb95de574a242c1c0912
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-02-10 22:13:41 +0530

    Time: 106 ms (35.20%), Space: 29 MB (17.20%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+int maxDistance(vector<vector<int>>& g, int steps = 0) {
+  queue<pair<int, int>> q, q1;
+  for (auto i = 0; i < g.size(); ++i)
+    for (auto j = 0; j < g[i].size(); ++j)
+      if (g[i][j] == 1)
+        q.push({ i - 1, j }), q.push({ i + 1, j }), q.push({ i, j - 1 }), q.push({ i, j + 1 });
+  while (!q.empty()) {
+    ++steps;
+    while (!q.empty()) {
+      int i = q.front().first, j = q.front().second;
+      q.pop();
+      if (i >= 0 && j >= 0 && i < g.size() && j < g[i].size() && g[i][j] == 0) {
+        g[i][j] = steps;
+        q1.push({ i - 1, j }), q1.push({ i + 1, j }), q1.push({ i, j - 1 }), q1.push({ i, j + 1 });
+      }
+    }
+    swap(q1, q);
+  }
+  return steps == 1 ? -1 : steps - 1;
+}
+};
