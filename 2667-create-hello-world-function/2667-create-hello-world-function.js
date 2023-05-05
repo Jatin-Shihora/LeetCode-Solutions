c00d4a064ce9d05e204b7a69bbffeed7172b0f9e
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-05 07:48:38 +0530

    Time: 57 ms (100.00%), Space: 41.7 MB (100.00%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,11 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+        return () => "Hello World";
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
