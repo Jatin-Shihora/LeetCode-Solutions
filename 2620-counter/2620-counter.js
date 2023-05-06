c0e0fccf4ba0c9047955a363ed17e8e18393ba41
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-06 06:36:54 +0530

    Time: 55 ms (69.57%), Space: 42.3 MB (26.12%) - LeetHub

---
+++ 2620-counter/2620-counter.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    return ()=> n++
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
