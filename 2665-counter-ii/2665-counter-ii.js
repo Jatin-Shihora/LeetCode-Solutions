e53df115c0afdc3cf524cc34db99fe4f5a6fe127
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-07 07:19:57 +0530

    Time: 78 ms (8.40%), Space: 44.6 MB (74.53%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -3,12 +3,21 @@
  * @return { increment: Function, decrement: Function, reset: Function }
  */
 var createCounter = function(init) {
-    let presentCount = init
-    return {
-        increment:()=> ++presentCount,
-        decrement:()=> --presentCount,
-        reset:()=> presentCount = init,
-    }
+  let presentCount = init;
+
+  function increment() {
+    return ++presentCount;
+  }
+
+  function decrement() {
+      return --presentCount;
+  }
+
+  function reset() {
+      return (presentCount = init);
+  }
+
+  return { increment, decrement, reset };
 };
 
 /**
