38e0b108830cfb125c62b0f849299201d10701fd
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-08 06:35:35 +0530

    Time: 59 ms (50.15%), Space: 42.3 MB (27.83%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -5,8 +5,8 @@
  */
 var map = function(arr, fn) {
   const transformedArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    transformedArr[i] = fn(arr[i], i);
-  }
+  arr.forEach((element, index) => {
+    transformedArr[index] = fn(element, index);
+  });
   return transformedArr;
 };
