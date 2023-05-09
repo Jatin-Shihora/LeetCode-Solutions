f183ac8425c6e7494c7099c6d6bd744307e27e46
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-09 07:53:13 +0530

    Time: 59 ms (51.50%), Space: 42.1 MB (50.33%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -4,11 +4,10 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-  const result = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (fn(arr[i], i)) {
-      result.push(arr[i]);
+  return arr.reduce((result, value, index) => {
+    if (fn(value, index)) {
+      result.push(value);
     }
-  }
-  return result;
+    return result;
+  }, []);
 };
