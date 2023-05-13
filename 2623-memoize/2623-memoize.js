c5e1bf617f1d05b22dd0359a5a7cae3aa72f0489
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-13 06:32:05 +0530

    Time: 355 ms (69.76%), Space: 107.3 MB (26.18%) - LeetHub

--- 2623-memoize/2623-memoize.js
+++ 2623-memoize/2623-memoize.js
@@ -2,14 +2,14 @@
  * @param {Function} fn
  */
 function memoize(fn) {
-  const cache = {};
+  const cacheObject = {};
   return function(...args) {
     const key = JSON.stringify(args);
-    if (key in cache) {
-      return cache[key];
+    if (key in cacheObject) {
+      return cacheObject[key];
     } else {
       const result = fn(...args);
-      cache[key] = result;
+      cacheObject[key] = result;
       return result;
     }
   }
