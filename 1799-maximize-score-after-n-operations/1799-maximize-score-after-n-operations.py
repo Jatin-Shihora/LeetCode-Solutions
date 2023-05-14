abbc35c5bd003d84e5889a0c614287ae1f289110
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-14 18:32:08 +0530

    Time: 3573 ms (22.79%), Space: 16.3 MB (74.68%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
@@ -2,7 +2,7 @@ class Solution:
     def maxScore(self, nums: List[int]) -> int:
         max_score = 0
         
-        def dfs(remaining, temp):
+        def run(remaining, temp):
             if len(remaining) == 2:
                 temp = sorted(temp + [gcd(*remaining)])
                 score = sum(i*a for i, a in enumerate(temp, start=1))
@@ -11,7 +11,7 @@ class Solution:
                     max_score = score
             else:
                 for i, r in enumerate(remaining[1:], start=1):
-                    dfs(remaining[1:i] + remaining[i+1:], temp + [gcd(remaining[0], r)])
+                    run(remaining[1:i] + remaining[i+1:], temp + [gcd(remaining[0], r)])
             
-        dfs(nums, [])
+        run(nums, [])
         return max_score
