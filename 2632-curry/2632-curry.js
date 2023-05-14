987f08ccabfd308fbeb0c0daeb435e2ad3632834
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-14 11:12:05 +0530

    Time: 66 ms (71.49%), Space: 48.9 MB (15.47%) - LeetHub

---
+++ 2632-curry/2632-curry.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length < fn.length) {
+      return function(...args2) {
+        return curried(...args, ...args2);
+      }
+    }
+    return fn(...args);
+  };
+}
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
