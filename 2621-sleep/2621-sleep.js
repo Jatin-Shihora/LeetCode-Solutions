97b5d298f3a8daccddac16a92908513f8ac7838b
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-15 08:21:33 +0530

    Time: 71 ms (5.70%), Space: 42.3 MB (10.50%) - LeetHub

--- 2621-sleep/2621-sleep.js
+++ 2621-sleep/2621-sleep.js
@@ -2,9 +2,14 @@
  * @param {number} millis
  */
 async function sleep(millis) {
-  return new Promise(delayresolve => setTimeout(delayresolve, millis));
+  return new Promise((resolve, reject) => {
+    if (typeof millis !== 'number' || isNaN(millis)) {
+      reject(new Error('Invalid argument. Expected a number.'));
+    } else {
+      setTimeout(resolve, millis);
+    }
+  });
 }
-
 /** 
  * let t = Date.now()
  * sleep(100).then(() => console.log(Date.now() - t)) // 100
