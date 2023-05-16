fadbfaf030cb9e1ad72d479398675d1b7841666c
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-16 09:15:53 +0530

    Time: 65 ms (53.10%), Space: 43.4 MB (16.32%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.ts
@@ -0,0 +1,26 @@
+type Fn = (...params: any[]) => Promise<any>;
+
+function timeLimit(fn: Fn, t: number): Fn {
+  return async function (...args: any[]) {
+    return new Promise((resolve, reject) => {
+      const timeoutId = setTimeout(() => {
+        clearTimeout(timeoutId);
+        reject("Time Limit Exceeded");
+      }, t);
+
+      fn(...args)
+        .then((result) => {
+          clearTimeout(timeoutId);
+          resolve(result);
+        })
+        .catch((error) => {
+          clearTimeout(timeoutId);
+          reject(error);
+        });
+    });
+  };
+}
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
