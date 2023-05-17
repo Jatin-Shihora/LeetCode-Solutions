02371af333330a0f108167eecd73a6e1d09d6658
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-17 07:46:38 +0530

    Time: 70 ms (21.34%), Space: 42.5 MB (15.32%) - LeetHub

---
+++ 2636-promise-pool/2636-promise-pool.js
@@ -0,0 +1,32 @@
+/**
+ * @param {Function[]} functions
+ * @param {number} n
+ * @return {Function}
+ */
+var promisePool = async function(functions, n) {
+  const results = [];
+  const inProgress = [];
+  let i = 0;
+
+  while (i < functions.length || inProgress.length > 0) {
+    while (inProgress.length < n && i < functions.length) {
+      const promise = functions[i]();
+      const index = i;
+      const resultPromise = promise.then((result) => {
+        results[index] = result;
+        inProgress.splice(inProgress.indexOf(resultPromise), 1);
+      });
+      inProgress.push(resultPromise);
+      i++;
+    }
+
+    await Promise.race(inProgress);
+  }
+
+  return results;
+};
+/**
+ * const sleep = (t) => new Promise(res => setTimeout(res, t));
+ * promisePool([() => sleep(500), () => sleep(400)], 1)
+ *   .then(console.log) // After 900ms
+ */
