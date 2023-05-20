0153ddd78265040f15827c88d192a16740e337d9
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-20 20:02:18 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (50.69%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+void dfs(string start,string end,map<string,double>& mp,map<string,vector<string>>& graph,double& val,map<string,int>& visited,bool& found){
+        visited[start]=1;
+    
+        if(found==true)
+            return ;
+        for(auto child:graph[start]){
+            if(visited[child]!=1){
+                // cout<<start<<" "<<child<<"\n";
+                val*=mp[start+"->"+child];
+                if(end==child){
+                    // cout<<end<<" -- "<<child<<"\n";
+                    found=true;
+                    return ;
+                }
+                dfs(child,end,mp,graph,val,visited,found);
+                if(found==true){
+                    return ;
+                }
+                else{
+                    val/=mp[start+"->"+child];
+                }
+            }
+        }
+       
+        
+    }
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        vector<double> ans;
+        map<string,double> mp;
+        map<string,vector<string>> graph;
+        for(int i=0;i<equations.size();i++){
+            string u=equations[i][0];
+            string v=equations[i][1];
+            mp[u+"->"+v]=values[i];
+            mp[v+"->"+u]=1/values[i];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+        for(int i=0;i<queries.size();i++){
+            string start=queries[i][0];
+            string end=queries[i][1];
+            if(graph.find(start)==graph.end()||graph.find(end)==graph.end()){
+                ans.push_back(-1);
+            }
+            else{
+                // ans.push_back(1);
+                double val=1;
+                map<string,int> visited;
+                bool found=false;
+                if(start==end){
+                    found=true;
+                }
+                else
+                dfs(start,end,mp,graph,val,visited,found);
+                if(found==true)
+                ans.push_back(val);
+                else
+                    ans.push_back(-1);
+                
+            }
+        }
+        return ans;
+    }
+};
