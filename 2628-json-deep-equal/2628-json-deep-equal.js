aa0e043779000b3c225a5e2bfd131b5defa8c95b
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-20 07:27:44 +0530

    Time: 88 ms (24.83%), Space: 47.4 MB (45.62%) - LeetHub

--- 2628-json-deep-equal/2628-json-deep-equal.js
+++ 2628-json-deep-equal/2628-json-deep-equal.js
@@ -3,16 +3,25 @@
  * @param {any} o2
  * @return {boolean}
  */
-function helper(key, value) {
-    if (value && typeof value === "object" && !Array.isArray(value))
-        return Object.fromEntries(Object.entries(value).sort());
-    else
-        return value;
+function areDeeplyEqual(o1, o2) {
+  var objs = [[o1, o2]];
+
+  while (objs.length) {
+    [o1, o2] = objs.pop();
+
+    if (o1 === o2) continue;
+    if (typeof o1 !== 'object' || typeof o2 !== 'object') return false;
+    if (Array.isArray(o1) !== Array.isArray(o2)) return false;
+
+    const keys1 = Object.keys(o1);
+    const keys2 = Object.keys(o2);
+
+    if (keys1.length !== keys2.length) return false;
+    for (const key of keys1) {
+      if (!(key in o2)) return false;
+      objs.push([o1[key], o2[key]]);
+    }
+  }
+
+  return true;
 }
-
-var areDeeplyEqual = function(o1, o2) {
-    const stringifiedO1 = JSON.stringify(o1, helper);
-    const stringifiedO2 = JSON.stringify(o2, helper);
-
-    return stringifiedO1 === stringifiedO2;
-};
