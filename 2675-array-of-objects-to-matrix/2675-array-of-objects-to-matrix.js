f0d48a6db2284f9d6e1332df446c9302f25a2b46
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-21 19:01:11 +0530

    Time: 603 ms (35.72%), Space: 133.9 MB (5.27%) - LeetHub

--- 2675-array-of-objects-to-matrix/2675-array-of-objects-to-matrix.js
+++ 2675-array-of-objects-to-matrix/2675-array-of-objects-to-matrix.js
@@ -1,54 +1,61 @@
 var jsonToMatrix = function(arr) {
-  const columnMap = new Map();
-  const result = [[]];
+  const isObject = (element) => element instanceof Object;
 
-  const sortColumns = (matrix) => {
-    const sortedColumnNames = matrix[0].sort((a, b) => a.localeCompare(b));
-    const copyMatrix = [[]];
-    for (let i = 1; i < matrix.length; i++) {
-      copyMatrix[i] = matrix[i].slice();
+  const getSubMap = (obj) => {
+    const map = new Map();
+    const setMap = (elem, preKey) => {
+      if (!isObject(elem)) {
+        map.set(preKey, elem);
+        return;
+      }
+
+      const subMap = getSubMap(elem);
+      for (const entry of subMap.entries()) {
+        const symbol = `${preKey}.${entry[0]}`;
+        map.set(symbol, entry[1]);
+      }
+    };
+
+    if (Array.isArray(obj)) {
+      for (let i = 0; i < obj.length; i++) {
+        setMap(obj[i], `${i}`);
+      }
+    } else {
+      for (const key of Object.keys(obj)) {
+        setMap(obj[key], key);
+      }
     }
 
-    for (let i = 0; i < matrix[0].length; i++) {
-      const oldColumn = columnMap.get(sortedColumnNames[i]);
-      if (oldColumn === i) {
-        continue;
-      }
-      for (let j = 1; j < matrix.length; j++) {
-        matrix[j][i] = copyMatrix[j][oldColumn];
-      }
-    }
-    return matrix;
+    return map;
   };
 
-  for (let i = 0; i < arr.length; i++) {
-    const stack = [[arr[i], []]];
-    result.push(Array(columnMap.size).fill(''));
-    while (stack.length > 0) {
-      const [front, path] = stack.pop();
-      if (typeof front === 'object' && front !== null) {
-        const keys = Object.keys(front);
-        for (let j = keys.length - 1; j >= 0; j--) {
-          stack.push([front[keys[j]], path.concat(keys[j])]);
-        }
-      } else if (Array.isArray(front)) {
-        for (let j = front.length - 1; j >= 0; j--) {
-          stack.push([front[j], path.concat(j)]);
-        }
-      } else {
-        const pathStr = path.join('.');
-        if (!columnMap.has(pathStr)) {
-          result[0].push(pathStr);
-          columnMap.set(pathStr, result[0].length - 1);
-          for (let j = 1; j < result.length; j++) {
-            result[j][result[0].length - 1] = '';
-          }
-        }
-        const j = columnMap.get(pathStr);
-        result[i + 1][j] = front;
+  const subMap = getSubMap(arr);
+  const columnSet = new Set();
+
+  for (const key of subMap.keys()) {
+    const i = key.indexOf('.');
+    const symbol = key.slice(i + 1);
+    columnSet.add(symbol);
+  }
+
+  const columnKeys = [...columnSet].sort((a, b) => (a < b ? -1 : 1));
+  const len = arr.length;
+  const matrix = [columnKeys];
+
+  for (let i = 1; i <= len; i++) {
+    if (columnKeys.length === 0) {
+      matrix[i] = [];
+      continue;
+    }
+
+    for (let j = 0; j < columnKeys.length; j++) {
+      const key = `${i - 1}.${columnKeys[j]}`;
+      if (!matrix[i]) {
+        matrix[i] = [];
       }
+      matrix[i][j] = subMap.has(key) ? subMap.get(key) : '';
     }
   }
 
-  return sortColumns(result);
+  return matrix;
 };
