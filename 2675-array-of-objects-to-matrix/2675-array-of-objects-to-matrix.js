dcfa406a843cd35a6dc03b1970e5a6b596b01592
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-21 19:24:46 +0530

    Time: 565 ms (40.23%), Space: 133.3 MB (5.27%) - LeetHub

--- 2675-array-of-objects-to-matrix/2675-array-of-objects-to-matrix.js
+++ 2675-array-of-objects-to-matrix/2675-array-of-objects-to-matrix.js
@@ -1,31 +1,66 @@
-var jsonToMatrix = function (arr) {
-  const isObject = x => (x !== null && typeof x === 'object')
+/**
+ * @param {Array} arr
+ * @return {Matrix}
+ */
+var jsonToMatrix = function(arr) {
+      const isObject = elem => elem instanceof Object
 
-  const getKeys = (arg) => {
-    if (!isObject(arg)) return ['']
-    return Object.keys(arg).reduce((acc, curr) => {
-      return (acc.push(...getKeys(arg[curr]).map(x => x ? `${curr}.${x}` : curr)), acc)
-    }, [])
-  }
+  const getSub = (obj) => {
+    const map = new Map()
 
-  const keys = [...arr.reduce((acc, curr) => {
-    getKeys(curr).forEach((k) => acc.add(k))
-    return acc
-  }, new Set())].sort()
+    const setMap = (elem, preKey) => {
+      if (!isObject(elem)) {
+        map.set(preKey, elem)
+        return
+      }
 
-  const getValue = (obj, path) => {
-    const paths = path.split('.')
-    let i = 0;
-    let value = obj
-    while (i < paths.length) {
-      if (!isObject(value)) break;
-      value = value[paths[i++]]
+      const subMap = getSub(elem)
+      for (const entry of subMap.entries()) {
+        const symbol = `${preKey}.${entry[0]}`
+        map.set(symbol, entry[1])
+      }
     }
-    if (i < paths.length || isObject(value) || value === undefined) return ''
-    return value
+
+    if (Array.isArray(obj)) {
+      for (let i = 0; i < obj.length; i++) {
+        setMap(obj[i], `${i}`)
+      }
+    } else {
+      for (const key of Object.keys(obj)) {
+        setMap(obj[key], key)
+      }
+    }
+
+    return map
   }
 
-  return arr.reduce((acc, curr) => {
-    return (acc.push(keys.map(key => getValue(curr, key))), acc)
-  }, [keys])
+  const map = getSub(arr)
+  const set = new Set()
+
+  for (const key of map.keys()) {
+    const i = key.indexOf('.')
+    const symbol = key.slice(i + 1)
+    set.add(symbol)
+  }
+
+  const keys = [...set].sort((a, b) => a < b ? -1 : 1)
+  const len = arr.length
+  const res = [keys]
+
+  for (let i = 1; i <= len; i++) {
+    if (keys.length === 0) {
+      res[i] = []
+      continue
+    }
+
+    for (let j = 0; j < keys.length; j++) {
+      const key = `${i - 1}.${keys[j]}`
+      if (!res[i]) {
+        res[i] = []
+      }
+      res[i][j] = map.has(key) ? map.get(key) : ""
+    }
+  }
+
+  return res
 };
