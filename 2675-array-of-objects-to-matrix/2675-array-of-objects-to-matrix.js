c138de4a72e692fa7ab42274b0d89edec6bfd2e6
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-21 19:06:25 +0530

    Time: 421 ms (72.93%), Space: 92.6 MB (92.11%) - LeetHub

--- 2675-array-of-objects-to-matrix/2675-array-of-objects-to-matrix.js
+++ 2675-array-of-objects-to-matrix/2675-array-of-objects-to-matrix.js
@@ -1,61 +1,31 @@
-var jsonToMatrix = function(arr) {
-  const isObject = (element) => element instanceof Object;
+var jsonToMatrix = function (arr) {
+  const isObject = x => (x !== null && typeof x === 'object')
 
-  const getSubMap = (obj) => {
-    const map = new Map();
-    const setMap = (elem, preKey) => {
-      if (!isObject(elem)) {
-        map.set(preKey, elem);
-        return;
-      }
-
-      const subMap = getSubMap(elem);
-      for (const entry of subMap.entries()) {
-        const symbol = `${preKey}.${entry[0]}`;
-        map.set(symbol, entry[1]);
-      }
-    };
-
-    if (Array.isArray(obj)) {
-      for (let i = 0; i < obj.length; i++) {
-        setMap(obj[i], `${i}`);
-      }
-    } else {
-      for (const key of Object.keys(obj)) {
-        setMap(obj[key], key);
-      }
-    }
-
-    return map;
-  };
-
-  const subMap = getSubMap(arr);
-  const columnSet = new Set();
-
-  for (const key of subMap.keys()) {
-    const i = key.indexOf('.');
-    const symbol = key.slice(i + 1);
-    columnSet.add(symbol);
+  const getKeys = (arg) => {
+    if (!isObject(arg)) return ['']
+    return Object.keys(arg).reduce((acc, curr) => {
+      return (acc.push(...getKeys(arg[curr]).map(x => x ? `${curr}.${x}` : curr)), acc)
+    }, [])
   }
 
-  const columnKeys = [...columnSet].sort((a, b) => (a < b ? -1 : 1));
-  const len = arr.length;
-  const matrix = [columnKeys];
+  const keys = [...arr.reduce((acc, curr) => {
+    getKeys(curr).forEach((k) => acc.add(k))
+    return acc
+  }, new Set())].sort()
 
-  for (let i = 1; i <= len; i++) {
-    if (columnKeys.length === 0) {
-      matrix[i] = [];
-      continue;
+  const getValue = (obj, path) => {
+    const paths = path.split('.')
+    let i = 0;
+    let value = obj
+    while (i < paths.length) {
+      if (!isObject(value)) break;
+      value = value[paths[i++]]
     }
-
-    for (let j = 0; j < columnKeys.length; j++) {
-      const key = `${i - 1}.${columnKeys[j]}`;
-      if (!matrix[i]) {
-        matrix[i] = [];
-      }
-      matrix[i][j] = subMap.has(key) ? subMap.get(key) : '';
-    }
+    if (i < paths.length || isObject(value) || value === undefined) return ''
+    return value
   }
 
-  return matrix;
+  return arr.reduce((acc, curr) => {
+    return (acc.push(keys.map(key => getValue(curr, key))), acc)
+  }, [keys])
 };
