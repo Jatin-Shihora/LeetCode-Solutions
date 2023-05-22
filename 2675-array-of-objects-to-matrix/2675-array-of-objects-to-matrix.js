b39919710915e98f36451d4e2fb2016908d6f559
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-22 17:53:05 +0530

    Time: 448 ms (64.54%), Space: 118.9 MB (20.21%) - LeetHub

--- 2675-array-of-objects-to-matrix/2675-array-of-objects-to-matrix.js
+++ 2675-array-of-objects-to-matrix/2675-array-of-objects-to-matrix.js
@@ -1,58 +1,47 @@
-var jsonToMatrix = function (arr) {
-  const colMap = new Map()
-  const res = [[]]
-
-    const sortCols = (matrix) => {
-      // Copy the column names from the first row of the matrix and sort them using localeCompare
-      const sortedColNames = matrix[0].slice().sort((a, b) => a.localeCompare(b));
-
-      // Create a new sorted matrix based on the sorted column names
-      const sortedMatrix = matrix.map((row) => {
-        const sortedRow = [];
-        for (let i = 0; i < sortedColNames.length; i++) {
-          const colIndex = matrix[0].indexOf(sortedColNames[i]);
-          sortedRow.push(row[colIndex]);
+/**
+ * @param {Array} arr
+ * @return {Matrix}
+ */
+function* getNestedColumn(obj, prefixes = []) {
+    if (obj != null && Array.isArray(obj)) {
+        for (let key = 0; key < obj.length; key += 1) {
+            prefixes.push(key);
+            yield* getNestedColumn(obj[key], prefixes);
+            prefixes.pop()
         }
-        return sortedRow;
-      });
-
-      return sortedMatrix;
-    };
-
-  for (let i = 0; i < arr.length; i++) {
-    const stack = [[arr[i], []]]
-    res.push(Array(colMap.size).fill(""))
-
-    while (stack.length > 0) {
-      const [front, path] = stack.pop()
-
-      if (typeof front === "object" && front !== null) {
-        const keys = Object.keys(front)
-
-        for (let j = keys.length - 1; j >= 0; j--) {
-          stack.push([front[keys[j]], path.concat(keys[j])])
-        }
-      } else if (Array.isArray(front)) {
-        for (let j = front.length - 1; j >= 0; j--) {
-          stack.push([front[j], path.concat(j)])
-        }
-      } else {
-        let pathStr = path.join('.')
-
-        if (!colMap.has(pathStr)) {
-          res[0].push(pathStr)
-          colMap.set(pathStr, res[0].length - 1)
-
-          for (let j = 1; j < res.length; j++) {
-            res[j][res[0].length - 1] = ""
-          }
-        }
-
-        let j = colMap.get(pathStr)
-        res[i + 1][j] = front
-      }
+    } else if (obj != null && typeof obj === 'object') {
+        for (const key of Object.keys(obj)) {
+            prefixes.push(key);
+            yield* getNestedColumn(obj[key], prefixes);
+            prefixes.pop()
+        }  
+    } else if (prefixes.length > 0) {
+        yield [prefixes.join('.'), obj];
     }
-  }
+}
 
-  return sortCols(res)
+var jsonToMatrix = function(arr) {
+    const output = new Array(arr.length + 1).fill(null).map(() => []);
+    const rows = new Array(arr.length).fill(null).map(() => new Map());
+    const uniqueColumns = new Set();
+    for (let row = 0; row < arr.length; row += 1) {
+        for (const [key, value] of getNestedColumn(arr[row])) {
+            rows[row].set(key, value);
+            uniqueColumns.add(key);
+        }
+    }
+
+    const columns = [...uniqueColumns].sort();
+    output[0] = columns;
+    for (let row = 0; row < arr.length; row += 1) {
+        for (const col of columns) {
+            if (rows[row].has(col)) {
+                output[row + 1].push(rows[row].get(col))
+            } else {
+                output[row + 1].push('')
+            }
+        }
+    }
+
+    return output;
 };
