2017fef6d457135b242678e5671e56503596ca7c
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-23 09:39:23 +0530

    Time: 70 ms (50.42%), Space: 45.7 MB (50.00%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.ts
+++ 2677-chunk-array/2677-chunk-array.ts
@@ -4,19 +4,13 @@
  * @return {Array[]}
  */
 function chunk(arr: any[], size: number): any[][] {
-  let chunkedArray: any[][] = [[]];
-  let temp: any[] = [];
-
-  for (let i = 0; i < arr.length; i = i + size) {
-    for (let j = 0; j < size; j++) {
-      temp[j] = arr[j + i];
-      if (j + i === arr.length) {
-        temp.splice(j);
-        break;
-      }
+  return arr.reduce((chunkedArray: any[][], element: any) => {
+    const lastChunk = chunkedArray[chunkedArray.length - 1];
+    if (!lastChunk || lastChunk.length === size) {
+      chunkedArray.push([element]);
+    } else {
+      lastChunk.push(element);
     }
-    chunkedArray = [...chunkedArray, [...temp]];
-  }
-
-  return chunkedArray.slice(1);
+    return chunkedArray;
+  }, []);
 }
