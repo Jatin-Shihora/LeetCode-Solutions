79dcae5dce101b9023365ea8ad892ebda2b7596c
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-23 09:18:45 +0530

    Time: 67 ms (42.15%), Space: 45.3 MB (10.64%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -4,19 +4,13 @@
  * @return {Array[]}
  */
 var chunk = function(arr, size) {
-  let chunkedArray = [[]];
-  let temp = [];
-  
-  for (let i = 0; i < arr.length; i = i + size) {
-    for (let j = 0; j < size; j++) {
-      temp[j] = arr[j + i];
-      if (j + i === arr.length) {
-        temp.splice(j);
-        break;
-      }
+  return arr.reduce((chunkedArray, element) => {
+    const lastChunk = chunkedArray[chunkedArray.length - 1];
+    if (!lastChunk || lastChunk.length === size) {
+      chunkedArray.push([element]);
+    } else {
+      lastChunk.push(element);
     }
-    chunkedArray = [...chunkedArray, [...temp]];
-  }
-  
-  return chunkedArray.slice(1);
+    return chunkedArray;
+  }, []);
 };
