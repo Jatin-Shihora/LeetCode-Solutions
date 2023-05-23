b485444f104c070f3de01d0e2f1ce0729ee7ad17
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-23 09:24:52 +0530

    Time: 58 ms (82.67%), Space: 44.5 MB (62.07%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -3,7 +3,9 @@
  * @param {number} size
  * @return {Array[]}
  */
-const chunk = (arr, size) =>
-  Array.from({ length: Math.ceil(arr.length / size) }, (_, index) =>
-    arr.slice(index * size, index * size + size)
-  );
+var chunk = function(arr, size) {
+  return Array.from({ length: Math.ceil(arr.length / size) }, function(_, index) {
+    return arr.slice(index * size, index * size + size);
+  });
+};
+
