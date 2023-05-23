364d2e3b5e5cf020422896cc3d25e63964d9a7b1
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-23 09:37:23 +0530

    Time: 59 ms (93.75%), Space: 44.9 MB (87.50%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.ts
+++ 2677-chunk-array/2677-chunk-array.ts
@@ -1,17 +1,15 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
 function chunk(arr: any[], size: number): any[][] {
   const chunkedArray: any[][] = [];
   let index = 0;
 
   while (index < arr.length) {
-    let count = size;
-    const temp: any[] = [];
-
-    while (count-- > 0 && index < arr.length) {
-      temp.push(arr[index]);
-      index++;
-    }
-
-    chunkedArray.push(temp);
+    chunkedArray.push(arr.slice(index, index + size));
+    index += size;
   }
 
   return chunkedArray;
