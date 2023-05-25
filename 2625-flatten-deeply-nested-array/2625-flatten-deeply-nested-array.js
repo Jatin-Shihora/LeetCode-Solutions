036c274b88f303bceda34212edd3ee508fb26ed4
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-05-25 16:29:02 +0530

    Time: 178 ms (62.99%), Space: 89.6 MB (34.16%) - LeetHub

--- 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -4,17 +4,18 @@
  * @return {any[]}
  */
 var flat = function (arr, n) {
-    let nested_array_element = true;
+    let nestedArrayElement = true;
+    let queue;
     let depth = 0;
 
-    while(nested_array_element && depth < n) {
-        nested_array_element = false;
+    while(nestedArrayElement && depth < n) {
+        nestedArrayElement = false;
         queue = [];
 
         for(let i = 0; i < arr.length; i++) {
             if(Array.isArray(arr[i])) {
                 queue.push(...arr[i]);
-                nested_array_element = true;
+                nestedArrayElement = true;
             } else {
                 queue.push(arr[i]);
             }
