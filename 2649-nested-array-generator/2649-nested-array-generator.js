d8a574e602670440907896b1bcff2b9e15b59646
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-06-01 15:36:54 +0530

    Time: 184 ms (73.04%), Space: 75.3 MB (43.40%) - LeetHub

--- 2649-nested-array-generator/2649-nested-array-generator.js
+++ 2649-nested-array-generator/2649-nested-array-generator.js
@@ -2,16 +2,17 @@
  * @param {Array} arr
  * @return {Generator}
  */
-var inorderTraversal = function*(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        let element = arr[i];
-        if (Array.isArray(element)) {
-            yield* inorderTraversal(element);
-        } else {
-            yield element;
-        }
+var inorderTraversal = function* (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let element = arr[i];
+    if (Array.isArray(element)) {
+      yield* inorderTraversal(element);
+    } else {
+      yield element;
     }
+  }
 };
+
 /**
  * const gen = inorderTraversal([1, [2, 3]]);
  * gen.next().value; // 1
