d4c0fac9db1616bed3cbd7323ce80d72796aa2c2
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-06-02 07:52:16 +0530

    Time: 163 ms (84.76%), Space: 66.6 MB (83.23%) - LeetHub

--- 2649-nested-array-generator/2649-nested-array-generator.ts
+++ 2649-nested-array-generator/2649-nested-array-generator.ts
@@ -1,24 +1,8 @@
 type MultidimensionalArray = (MultidimensionalArray | number)[];
 
 function* inorderTraversal(arr: MultidimensionalArray): Generator<number, void, unknown> {
-  if (arr.length === 0) {
-    return null;
-  }
-
-  const stack: MultidimensionalArray[] = [arr];
-  while (stack.length > 0) {
-    const current = stack.pop();
-    if (Array.isArray(current)) {
-      for (let i = current.length - 1; i >= 0; i--) {
-        stack.push(current[i] as MultidimensionalArray); // Type assertion added here
-      }
-    } else {
-      yield current as number;
-    }
-  }
+  yield* (arr.flat(Infinity) as unknown as Generator<number, void, unknown>);
 }
-
-
 
 
 /**
