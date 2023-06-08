9a066b6476d375a547541378faf848cbb2b24a18
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-06-08 23:23:32 +0530

    Time: 220 ms (57.87%), Space: 65.7 MB (9.48%) - LeetHub

--- 2624-snail-traversal/2624-snail-traversal.js
+++ 2624-snail-traversal/2624-snail-traversal.js
@@ -1,6 +1,9 @@
 Array.prototype.snail = function(rowsCount, colsCount) {
+    // base case
     if(this.length !==  rowsCount * colsCount) return [];
+    // support variable
     let result = Array(rowsCount).fill([]).map(() => []);
+   // populating res
     for(let j = 0; j < this.length; j++){
         const i = Math.floor(j / rowsCount);
         if(i % 2 === 0){
