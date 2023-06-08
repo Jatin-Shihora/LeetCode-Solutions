fed470811d5f0f0e5d3b419770ffbfec43c08c29
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-06-08 23:24:51 +0530

    Time: 233 ms (41.00%), Space: 63.5 MB (52.68%) - LeetHub

--- 2624-snail-traversal/2624-snail-traversal.ts
+++ 2624-snail-traversal/2624-snail-traversal.ts
@@ -5,28 +5,23 @@ declare global {
 }
 
 Array.prototype.snail = function(rowsCount: number, colsCount: number): number[][] {
-    // base case: wrong inputs
-    if (rowsCount * colsCount !== this.length) return [];
-    // support variables
-    const res = Array.from({length: rowsCount}, () => Array.from({length: colsCount}) as number[]);
-    let goingDown = true, x = 0, y = 0;
-    // populating res
-    for (const n of this) {
-      res[y][x] = n;
-      // updating the pointers
-      if (goingDown) {
-        y++;
-        // end of the line
-        if (y === rowsCount) y--, x++, goingDown = false;
-      }
-      else  {
-        y--;
-        // end of the line
-        if (y < 0) y = 0, x++, goingDown = true;
-      }
+  // base case
+  if (this.length !== rowsCount * colsCount) return [];
+  // support variable
+  let result: number[][] = Array(rowsCount).fill([]).map(() => []);
+  // populating res
+  for (let j = 0; j < this.length; j++) {
+    const i = Math.floor(j / rowsCount);
+    if (i % 2 === 0) {
+      result[j % rowsCount][i] = this[j];
+    } else {
+      result[rowsCount - j % rowsCount - 1][i] = this[j];
     }
-    return res;
+  }
+  return result;
 }
+
+
 /**
  * const arr = [1,2,3,4];
  * arr.snail(1,4); // [[1,2,3,4]]
