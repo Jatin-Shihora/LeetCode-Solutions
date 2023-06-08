217df2acd0b2dad7c1d0b970ede3f2ed8f0897fc
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-06-08 23:43:45 +0530

    Time: 208 ms (90.61%), Space: 63.7 MB (49.62%) - LeetHub

--- 2624-snail-traversal/2624-snail-traversal.ts
+++ 2624-snail-traversal/2624-snail-traversal.ts
@@ -6,23 +6,18 @@ declare global {
 
 Array.prototype.snail = function(rowsCount: number, colsCount: number): number[][] {
   // base case
-  if (this.length !== rowsCount * colsCount) return [];
+  if (rowsCount * colsCount !== this.length) return [];
   // support variable
   let result: number[][] = Array(rowsCount).fill([]).map(() => []);
   // populating res
-  for (let j = 0; j < this.length; j++) {
-    const i = Math.floor(j / rowsCount);
-    if (i % 2 === 0) {
-      result[j % rowsCount][i] = this[j];
-    } else {
-      result[rowsCount - j % rowsCount - 1][i] = this[j];
+  for (let row = 0; row < colsCount; row++) {
+    for (let col = 0; col < rowsCount; col++) {
+      if (row & 1) {
+        result[rowsCount - col - 1].push(this[row * rowsCount + col]);
+      } else {
+        result[col].push(this[row * rowsCount + col]);
+      }
     }
   }
   return result;
 }
-
-
-/**
- * const arr = [1,2,3,4];
- * arr.snail(1,4); // [[1,2,3,4]]
- */
