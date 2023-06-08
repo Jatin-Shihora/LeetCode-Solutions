71fb38d2926d7d182042f38d61d9ef75b2b57d23
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-06-08 23:22:41 +0530

    Time: 226 ms (42.71%), Space: 65.6 MB (9.48%) - LeetHub

--- 2624-snail-traversal/2624-snail-traversal.js
+++ 2624-snail-traversal/2624-snail-traversal.js
@@ -1,30 +1,13 @@
 Array.prototype.snail = function(rowsCount, colsCount) {
-    // base case: wrong inputs
-    if (rowsCount * colsCount !== this.length) return [];
-    // support variables
-    let res = Array(rowsCount).fill().map(() => []);
-    let goingDown = true, x = 0, y = 0;
-    // populating res
-    for (const n of this) {
-        res[y][x] = n;
-        // updating the pointers
-        if (goingDown) {
-            y++;
-            // end of the line
-            if (y === rowsCount) {
-                y--;
-                x++;
-                goingDown = false;
-            }
-        } else {
-            y--;
-            // end of the line
-            if (y < 0) {
-                y = 0;
-                x++;
-                goingDown = true;
-            }
+    if(this.length !==  rowsCount * colsCount) return [];
+    let result = Array(rowsCount).fill([]).map(() => []);
+    for(let j = 0; j < this.length; j++){
+        const i = Math.floor(j / rowsCount);
+        if(i % 2 === 0){
+            result[j % rowsCount][i] = this[j];
+        }else{
+            result[rowsCount - j % rowsCount - 1][i] = this[j];
         }
     }
-    return res;
-};
+    return result;
+}
