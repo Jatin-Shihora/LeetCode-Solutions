48c02a157b4db24707c61ae2c9b26216d4cf6395
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-06-12 23:57:14 +0530

    Time: 82 ms (16.39%), Space: 43.4 MB (18.03%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.ts
+++ 2723-add-two-promises/2723-add-two-promises.ts
@@ -1,6 +1,9 @@
 async function addTwoPromises(promise1: Promise<number>, promise2: Promise<number>): Promise<number> {
   try {
-    return await promise1 + await promise2;
+    return promise1.then((val) => promise2.then((val2) => val + val2)).catch((error) => {
+      console.error(error);
+      throw error; // Rethrow the error to maintain the behavior of propagating the error to the caller
+    });
   } catch (error) {
     console.error(error);
     throw error; // Rethrow the error to maintain the behavior of propagating the error to the caller
