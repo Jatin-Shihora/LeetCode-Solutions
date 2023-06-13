3b1996a46553dbbd863e26f154404ed54cdce184
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-06-13 09:03:18 +0530

    Time: 69 ms (51.39%), Space: 42.1 MB (45.83%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -1,31 +1,11 @@
-/**
- * @param {Promise} promise1
- * @param {Promise} promise2
- * @return {Promise}
- */
-var addTwoPromises = async function (promise1, promise2) {
-  return new Promise((resolve, reject) => {
-    let count = 2;
-    let res = 0; 
-    
-    [promise1, promise2].forEach(async promise => {
-      try {
-        const subRes = await promise;
-        res += subRes;
-        count--;
-
-        if (count === 0) {
-          resolve(res);
-        }
-      } catch (err) {
-        reject(err);
-      }
-    });
-  });
+var addTwoPromises = async function(promise1, promise2) {
+  try {
+    return promise1.then((val) => promise2.then((val2) => val + val2))
+  } catch (error) {
+    console.error(error);
+    throw error; // Rethrow the error to maintain the behavior of propagating the error to the caller
+  }
 };
-
-
-
 /**
  * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
  *   .then(console.log); // 4
