3da0c789b03e73fe622e3275010b83389730e09c
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-06-26 10:55:50 +0530

    Time: 71 ms (52.20%), Space: 42.3 MB (19.92%) - LeetHub

---
+++ 2715-execute-cancellable-function-with-delay/2715-execute-cancellable-function-with-delay.js
@@ -0,0 +1,34 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+  const timeoutId = setTimeout(function() {
+    fn.apply(this, args);
+  }, t);
+
+  const cancelFn = function() {
+    clearTimeout(timeoutId);
+  };
+
+  return cancelFn;
+};
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 5
+ *  const args = [2], t = 20, cancelT = 50
+ *
+ *  const log = (...argsArr) => {
+ *      result.push(fn(...argsArr))
+ *  }
+ *       
+ *  const cancel = cancellable(fn, args, t);
+ *           
+ *  setTimeout(() => {
+ *     cancel()
+ *     console.log(result) // [{"time":20,"returned":10}]
+ *  }, cancelT)
+ */
