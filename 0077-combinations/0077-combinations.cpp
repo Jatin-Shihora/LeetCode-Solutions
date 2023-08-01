699cb4a48257cdbb46b56064bff1936f56eeee96
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-08-01 07:38:26 +0530

    Time: 4 ms (99.44%), Space: 9.1 MB (61.59%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -3,18 +3,19 @@ class Solution {
   vector < vector < int >> combine(int n, int k) {
     vector < vector < int >> ans;
     vector < int > temp;
-    combine(1, n, k, ans, temp);
+    combine(1, n, k, ans, temp); //call function to get combination of k numberswhich range is 1-n
     return ans;
   }
   private: 
-  void combine(int begin, int n, int k, vector < vector < int >> & ans, vector < int > & temp){
+  void combine(int begin, int n, int k, vector < vector < int >> & ans, vector < int > & temp) {
     if (k == 0) {
       ans.push_back(temp);
       return;
     }
-    for (int i = begin; n - i + 1 >= k; i++) {
+    //condition : n-i+1 is the range, range must greater than k
+    for (int i = begin; n - i + 1 >= k; i++) { // for the ith iteration, get the combination of i and k-1 numbers differ from i.
       temp.push_back(i);
-      combine(i + 1, n, k - 1, ans, temp);
+      combine(i + 1, n, k - 1, ans, temp); //get the combination of k-1 numbers which range is (i+1,n)
       temp.pop_back();
     }
   }
