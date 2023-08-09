50254de477d19810f92ad4c36a391165fe43dd01
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-08-09 17:01:13 +0530

    Time: 40 ms (100%), Space: 44.4 MB (32.21%) - LeetHub

---
+++ 2768-array-upper-bound/2768-array-upper-bound.js
@@ -0,0 +1,29 @@
+/** 
+ * @param {number} target
+ * @return {number}
+ */
+Array.prototype.upperBound = function(target) {
+    let left = 0;
+    let right = this.length - 1;
+    let result = -1; // Initialize result as -1 to handle cases where target is not found
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (this[mid] === target) {
+            result = mid; // Update result to store the current index
+            left = mid + 1; // Continue searching in the right half for the last occurrence
+        } else if (this[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return result;
+};
+
+
+// [3,4,5].upperBound(5); // 2
+// [1,4,5].upperBound(2); // -1
+// [3,4,6,6,6,6,7].upperBound(6) // 5
