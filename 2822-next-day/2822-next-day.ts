2adbaae5153f36dc6f0233c70370432b700e9a35
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-08-09 11:47:19 +0530

    Time: 43 ms (97.3%), Space: 43.1 MB (75.68%) - LeetHub

--- 2822-next-day/2822-next-day.ts
+++ 2822-next-day/2822-next-day.ts
@@ -5,11 +5,32 @@ declare global {
 }
 
 Date.prototype.nextDay = function() {
-    const next = this.getTime() + 24 * 60 * 60 * 1000;
-    return new Date(next).toISOString().split("T")[0];
-};
+    const daysInMonth: number[] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    
+    const year: number = this.getFullYear();
+    const month: number = this.getMonth();
+    const day: number = this.getDate();
+    
+    const isLeapYear: boolean = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+    if (isLeapYear) {
+        daysInMonth[1] = 29;
+    }
 
-/**
- * const date = new Date("2014-06-20");
- * date.nextDay(); // "2014-06-21"
- */
+    const maxDays: number = daysInMonth[month];
+    
+    let nextYear: number = year;
+    let nextMonth: number = month;
+    let nextDay: number = day + 1;
+
+    if (nextDay > maxDays) {
+        nextDay = 1;
+        nextMonth += 1;
+        if (nextMonth > 11) {
+            nextMonth = 0;
+            nextYear += 1;
+        }
+    }
+
+    const formattedDate: string = `${nextYear}-${String(nextMonth + 1).padStart(2, '0')}-${String(nextDay).padStart(2, '0')}`;
+    return formattedDate;
+};
