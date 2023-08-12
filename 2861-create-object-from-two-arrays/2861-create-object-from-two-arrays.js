9518c70a364c5163fbf9c4057488c6498b4aa733
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-08-12 10:55:26 +0530

    Time: 101 ms (36%), Space: 55.6 MB (71.2%) - LeetHub

--- 2861-create-object-from-two-arrays/2861-create-object-from-two-arrays.js
+++ 2861-create-object-from-two-arrays/2861-create-object-from-two-arrays.js
@@ -3,12 +3,9 @@
  * @param {Array} valuesArr
  * @return {Object}
  */
-var createObject = function(keysArr, valuesArr) {
-    const obj = {};
-    keysArr.forEach((key, index) => {
-        if (!(key in obj)) {
-            obj[key] = valuesArr[index];
-        }
-    });
-    return obj;
+var createObject = function(K, V, O = {}) {
+    for(i in K)
+        if(!O.hasOwnProperty(K[i]))
+            O[K[i]] = V[i]
+    return O
 };
