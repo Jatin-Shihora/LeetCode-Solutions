9316125b06b08d5079afb023ab902a1bb76ac46e
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-08-13 12:08:15 +0530

    Time: 42 ms (91.06%), Space: 41.2 MB (86.18%) - LeetHub

--- 2901-factorial-generator/2901-factorial-generator.js
+++ 2901-factorial-generator/2901-factorial-generator.js
@@ -1,10 +1,16 @@
 function* factorial(n) {
+    function factorialRecursive(n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorialRecursive(n - 1);
+    }
+    
     if (n === 0) {
         yield 1;
-    }
-    let fact = 1;
-    for (let i = 1; i <= n; i++) {
-        fact *= i;
-        yield fact;
+    } else {
+        for (let i = 1; i <= n; i++) {
+            yield factorialRecursive(i);
+        }
     }
 }
