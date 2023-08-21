6459524049093573c1a3d7a9e2716dc4270ce73b
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-08-21 10:41:47 +0530

    Time: 11 ms (95.17%), Space: 12.7 MB (74.62%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string str) {
+        int i = 1, j = 0, n = str.size();
+        vector<int> dp(n+1,0);
+        while( i < str.size() ){
+            if( str[i] == str[j] ) dp[++i]=++j;
+            else if( j == 0 ) i++;
+            else j = dp[j];
+        }
+        return dp[n]&&dp[n]%(n-dp[n])==0;
+    }
+};
