97164cf38715f249161809494556f84ca3d9f56f
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-08-29 12:50:34 +0530

    Time: 128 ms (95.71%), Space: 59.9 MB (98.57%) - LeetHub

--- 2912-deep-object-filter/2912-deep-object-filter.js
+++ 2912-deep-object-filter/2912-deep-object-filter.js
@@ -1,46 +1,19 @@
-function deepFilter(obj, fn) {
-    function dfs(node) {
-        if (node === null) {
-            if (fn(node)) return node;
-            return undefined;
+var deepFilter = function(obj, fn) {
+    if(Array.isArray(obj)){
+        obj = obj.map((val) => deepFilter(val, fn)).filter((val) => val !== undefined)
+        
+        return obj.length > 0 ? obj : undefined
+    } else if(typeof obj === "object" && obj !== null){
+        const keys = Object.keys(obj)
+        let empty = true
+        
+        for(const key of keys){
+           obj[key] = deepFilter(obj[key], fn)
+           if (obj[key] !== undefined) empty = false 
         }
-        if (typeof node !== 'object') {
-            if (fn(node)) return node;
-            return undefined;
-        }
-
-        if (Array.isArray(node)) {
-            const newArr = [];
-
-            for (let i = 0; i < node.length; i++) {
-                const curr = node[i];
-                const subRes = dfs(curr);
-
-                if (subRes !== undefined) {
-                    newArr.push(subRes);
-                }
-            }
-
-            if (newArr.length === 0) {
-                return undefined;
-            }
-
-            return newArr;
-        }
-
-        const newObj = {};
-
-        for (const key in node) {
-            const subRes = dfs(node[key]);
-            if (subRes !== undefined) {
-                newObj[key] = subRes;
-            }
-        }
-
-        if (Object.keys(newObj).length === 0) return undefined;
-
-        return newObj;
+        
+        return empty ? undefined : obj
     }
-
-    return dfs(obj);
-}
+    
+    return fn(obj) ? obj : undefined
+};
