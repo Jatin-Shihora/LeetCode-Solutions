1042d60687f70f9eef451eeab648650458d4cd7a
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-08-29 12:53:32 +0530

    Time: 158 ms (20%), Space: 57.8 MB (100%) - LeetHub

--- 2912-deep-object-filter/2912-deep-object-filter.js
+++ 2912-deep-object-filter/2912-deep-object-filter.js
@@ -1,19 +1,19 @@
-var deepFilter = function(obj, fn) {
-    const dfs = (currentObj) => {
-        if (Array.isArray(currentObj)) {
-            const filteredArr = currentObj.map((val) => dfs(val)).filter((val) => val !== undefined);
-            return filteredArr.length > 0 ? filteredArr : undefined;
-        } else if (typeof currentObj === "object" && currentObj !== null) {
-            const keys = Object.keys(currentObj);
-            let empty = true;
-            for (const key of keys) {
-                currentObj[key] = dfs(currentObj[key]);
-                if (currentObj[key] !== undefined) empty = false;
-            }
-            return empty ? undefined : currentObj;
-        }
-        return fn(currentObj) ? currentObj : undefined;
-    };
-    
-    return dfs(obj);
+var deepFilter = function (obj, fn) {
+   const dfs = (currentObj) => {
+      if (Array.isArray(currentObj)) {
+         const filteredArr = currentObj.map((val) => dfs(val)).filter((val) => val !== undefined);
+         return filteredArr.length > 0 ? filteredArr : undefined;
+      } else if (typeof currentObj === "object" && currentObj !== null) {
+         const keys = Object.keys(currentObj);
+         let empty = true;
+         for (const key of keys) {
+            currentObj[key] = dfs(currentObj[key]);
+            if (currentObj[key] !== undefined) empty = false;
+         }
+         return empty ? undefined : currentObj;
+      }
+      return fn(currentObj) ? currentObj : undefined;
+   };
+
+   return dfs(obj);
 };
