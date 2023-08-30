a9b70da5dd8144bab4e3bd6f8ae5df990c3f95c4
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-08-30 12:09:42 +0530

    Time: 55 ms (64.93%), Space: 42.2 MB (62.45%) - LeetHub

---
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+  return nums.reduce(fn, init);
+};
