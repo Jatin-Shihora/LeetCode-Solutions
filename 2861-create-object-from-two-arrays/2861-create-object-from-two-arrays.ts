a952518775b4c211d2683e236fde3792eef131a3
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-09-15 17:19:51 +0530

    Time: 112 ms (8.33%), Space: 56.2 MB (91.67%) - LeetHub

--- 2861-create-object-from-two-arrays/2861-create-object-from-two-arrays.ts
+++ 2861-create-object-from-two-arrays/2861-create-object-from-two-arrays.ts
@@ -1,15 +1,12 @@
 function createObject(keysArr: (string | number | boolean)[], valuesArr: any[]): Record<string, any> {
     const obj: Record<string, any> = {};
-    const addedKeys: Set<string> = new Set();
 
     keysArr.forEach((key, index) => {
-        // Convert non-string keys to strings
         if (typeof key !== 'string') {
             key = String(key);
         }
 
-        if (!addedKeys.has(key)) {
-            addedKeys.add(key);
+        if(!(key in obj)) {
             obj[key] = valuesArr[index];
         }
     });
