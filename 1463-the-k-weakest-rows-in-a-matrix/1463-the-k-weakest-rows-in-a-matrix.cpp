991a16dde59e7f08d7ff1e739a17790302e2e944
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-09-18 12:13:15 +0530

    Time: 13 ms (31.65%), Space: 10.7 MB (13.97%) - LeetHub

---
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        int n=mat[0].size();
+        for(int i=0; i<mat.size(); i++){
+            mat[i].push_back(i);
+        }
+        sort(mat.begin(), mat.end());
+        vector<int> ans(k);
+        for(int i=0; i<k; i++){
+            ans[i]=mat[i][n];            
+        }
+        return ans;
+    }
+};
