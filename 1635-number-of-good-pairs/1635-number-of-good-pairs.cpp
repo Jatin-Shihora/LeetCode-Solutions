b32874269e97ed40c6dcdc544a59f569cc6b60b7
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-10-03 19:02:23 +0530

    Time: 0 ms (100%), Space: 7.6 MB (16.85%) - LeetHub

---
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& A) {
+        int res = 0;
+        unordered_map<int, int> count;
+        for (int a: A) {
+            res += count[a]++;
+        }
+        return res;
+    }
+};
