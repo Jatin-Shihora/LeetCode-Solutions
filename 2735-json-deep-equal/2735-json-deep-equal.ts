3d2f278fc6942f7728f8d09f615bbe32e8f51407
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-10-05 10:40:51 +0530

    Time: 92 ms (8.24%), Space: 47.5 MB (36.47%) - LeetHub

--- 2735-json-deep-equal/2735-json-deep-equal.ts
+++ 2735-json-deep-equal/2735-json-deep-equal.ts
@@ -1,14 +1,9 @@
-/**
- * @param {any} o1
- * @param {any} o2
- * @return {boolean}
- */
-function areDeeplyEqual(o1, o2) {
-  if (o1 === o2) return true;
+function areDeeplyEqual(o1: any, o2: any): boolean {
+  if (o1 === o2) return true; 
   if (o1 === null || o2 === null) return false;
-  var objs = [[o1, o2]];
+  const objs: [any, any][] = [[o1, o2]];
   while (objs.length) {
-    [o1, o2] = objs.pop();
+    [o1, o2] = objs.pop()!;
     if (o1 === o2) continue;
     if (typeof o1 !== 'object' || typeof o2 !== 'object') return false;
     if (Array.isArray(o1) !== Array.isArray(o2)) return false;
