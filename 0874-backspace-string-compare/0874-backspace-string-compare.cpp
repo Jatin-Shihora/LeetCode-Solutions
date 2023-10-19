c881ce401374a56f5ad9da0281d46698221b386b
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-10-19 07:56:56 +0530

    Time: 3 ms (47.11%), Space: 6.9 MB (15.59%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+/*
+- Scan the String from right to left. Maintain the "#" count. Increment 'count' variable if the character encountered is '#'. Otherwise decrement the count and if the count is 0, append that character to the result.
+- Perform above for both the strings, compare the results and return true/false accordingly.
+*/
     string getString(string &str){
         int n = str.length(), count = 0; 
         string result = "";
