efd6a14d925eb5c88097f051170afedc23a25aad
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-11-29 21:10:12 +0530

    Time: 63 ms (52.28%), Space: 41.7 MB (82.04%) - LeetHub

--- 2725-interval-cancellationdiv-classcss-1x6t3yla-titleedit-this-question-href-library-3025-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-a-div/2725-interval-cancellationdiv-classcss-1x6t3yla-titleedit-this-question-href-library-3025-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-a-div.js
+++ 2725-interval-cancellationdiv-classcss-1x6t3yla-titleedit-this-question-href-library-3025-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-a-div/2725-interval-cancellationdiv-classcss-1x6t3yla-titleedit-this-question-href-library-3025-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-a-div.js
@@ -5,9 +5,19 @@
  * @return {Function}
  */
 var cancellable = function(fn, args, t) {
+    let isCancelled = false;
     fn(...args);
-    const timer = setInterval(() => fn(...args), t);
+    const startInterval = () => {
+        setTimeout(() => {
+            if (isCancelled) return;
+            fn(...args);
+            startInterval();
+        }, t);
+    }
+    startInterval();
+    const cancelInterval = () => {
+        isCancelled = true;
+    }
 
-    const cancelFn = () => clearInterval(timer);
-    return cancelFn;
+    return cancelInterval;
 };
