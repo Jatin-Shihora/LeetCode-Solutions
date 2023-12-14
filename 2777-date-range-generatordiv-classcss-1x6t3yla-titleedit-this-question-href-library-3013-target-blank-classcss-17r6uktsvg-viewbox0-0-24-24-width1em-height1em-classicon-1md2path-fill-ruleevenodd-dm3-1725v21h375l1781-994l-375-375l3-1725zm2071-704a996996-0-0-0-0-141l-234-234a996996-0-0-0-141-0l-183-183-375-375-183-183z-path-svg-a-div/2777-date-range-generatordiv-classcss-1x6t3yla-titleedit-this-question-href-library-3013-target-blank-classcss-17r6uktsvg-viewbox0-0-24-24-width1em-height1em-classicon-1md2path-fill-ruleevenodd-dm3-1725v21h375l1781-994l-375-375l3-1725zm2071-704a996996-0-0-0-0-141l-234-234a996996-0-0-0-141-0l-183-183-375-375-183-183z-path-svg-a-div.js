7d9e10b3757b87b177345f07bb52d39c21e5ed4b
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-12-14 11:03:06 +0530

    Time: 55 ms (100.00%), Space: 44.9 MB (28.00%) - LeetHub

---
+++ 2777-date-range-generatordiv-classcss-1x6t3yla-titleedit-this-question-href-library-3013-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-a-div/2777-date-range-generatordiv-classcss-1x6t3yla-titleedit-this-question-href-library-3013-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-a-div.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} start
+ * @param {string} end
+ * @param {number} step
+ * @yields {string}
+ */
+ 
+var dateRangeGenerator = function dateRangeGenerator(start, end, step) {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+    const datesList = [];
+
+    while (startDate <= endDate) {
+        datesList.push(formatDate(startDate));
+        startDate.setDate(startDate.getDate() + step);
+    }
+
+    return datesList;
+}
+
+// Helper function to format the date
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
+
+/**
+ * const g = dateRangeGenerator('2023-04-01', '2023-04-04', 1);
+ * g.next().value; // '2023-04-01'
+ * g.next().value; // '2023-04-02'
+ * g.next().value; // '2023-04-03'
+ * g.next().value; // '2023-04-04'
+ * g.next().done; // true
+ */
