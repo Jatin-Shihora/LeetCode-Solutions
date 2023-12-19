02a3e18958cd4789dc9af6acc96666524ee9c6d6
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-12-19 09:56:43 +0530

    Time: 50 ms (23.08%), Space: 22 MB (59.38%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        // Save the dimensions of the image.
+        int m = img.size();
+        int n = img[0].size();
+
+        // Iterate over the cells of the image.
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // Initialize the sum and count 
+                int sum = 0;
+                int count = 0;
+
+                // Iterate over all plausible nine indices.
+                for (int x = i - 1; x <= i + 1; x++) {
+                    for (int y = j - 1; y <= j + 1; y++) {
+                        // If the indices form valid neighbor
+                        if (0 <= x && x < m && 0 <= y && y < n) {
+                            // Extract the original value of img[x][y].
+                            sum += img[x][y] & 255;
+                            count += 1;
+                        }
+                    }
+                }
+                
+                // Encode the smoothed value in img[i][j].
+                img[i][j] |= (sum / count) << 8;
+            }
+        }
+
+        // Extract the smoothed value from encoded img[i][j].
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                img[i][j] >>= 8;
+            }
+        }
+
+        // Return the smooth image.
+        return img;
+    }
+};
