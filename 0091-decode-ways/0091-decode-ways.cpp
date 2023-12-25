ac7b463f8f5b3c411dec19444e4e07742b2fa028
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2023-12-25 08:07:30 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (65.44%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        // edge case - leading zero and single character string
+        if(s[0] == '0') return 0;
+        if(s.size()==1) return 1;
+
+        int len = s.size(), dp[len];
+
+        dp[0] = 1;
+        dp[1] = (s[0] == '1' || s[0] == '2' && s[1] < '7' ? 1 : 0) + (s[1] != '0');
+        for(int i = 2; i<len; i++){
+            //edge case - we quite for two consecutive zeros
+            if (s[i] == '0' && (s[i - 1] > '2' || s[i - 1] == '0')) return 0;
+            //base case - we always keep the previous set of combinations, unless we met a 0
+            dp[i] = s[i] != '0' ? dp[i - 1] : 0;
+            // we go and look 2 positions behind if we can make a digit in the 10-26 range
+            if (s[i - 1] == '1' || s[i - 1] == '2' && s[i] < '7') dp[i] += dp[i - 2];
+        }
+        return dp[len-1];
+    }
+};
