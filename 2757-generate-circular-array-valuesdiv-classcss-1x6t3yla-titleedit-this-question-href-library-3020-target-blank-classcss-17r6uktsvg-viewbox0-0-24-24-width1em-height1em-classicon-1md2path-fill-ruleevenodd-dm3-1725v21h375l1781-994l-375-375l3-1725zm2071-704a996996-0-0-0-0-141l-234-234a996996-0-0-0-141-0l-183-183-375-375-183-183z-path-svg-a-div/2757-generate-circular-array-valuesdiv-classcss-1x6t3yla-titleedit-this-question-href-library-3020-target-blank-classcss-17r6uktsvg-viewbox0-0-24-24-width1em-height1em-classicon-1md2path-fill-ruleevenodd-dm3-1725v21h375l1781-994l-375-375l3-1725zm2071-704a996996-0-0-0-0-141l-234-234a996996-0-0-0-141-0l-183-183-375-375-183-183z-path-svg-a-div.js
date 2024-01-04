b0a53825f3f603b3798eba24f3959a16c33d9116
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2024-01-04 19:48:10 +0530

    Time: 57 ms (36.49%), Space: 42.1 MB (72.97%) - LeetHub

--- 2757-generate-circular-array-valuesdiv-classcss-1x6t3yla-titleedit-this-question-href-library-3020-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-a-div/2757-generate-circular-array-valuesdiv-classcss-1x6t3yla-titleedit-this-question-href-library-3020-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-a-div.js
+++ 2757-generate-circular-array-valuesdiv-classcss-1x6t3yla-titleedit-this-question-href-library-3020-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-a-div/2757-generate-circular-array-valuesdiv-classcss-1x6t3yla-titleedit-this-question-href-library-3020-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-a-div.js
@@ -1,7 +1,6 @@
 var cycleGenerator = function* (arr, startIndex) {
-  let index = startIndex;
-  while (true) {
-    const jump = yield arr[index];
-    index = (index + arr.length + jump % arr.length) % arr.length;
-  }
+    while (true) {
+        const jump = yield arr.at(startIndex % arr.length);
+        startIndex += jump;
+    }
 }
