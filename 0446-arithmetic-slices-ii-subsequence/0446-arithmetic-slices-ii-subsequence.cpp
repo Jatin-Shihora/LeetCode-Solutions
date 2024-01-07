c62680b89a0e9fdb799e2175837303a134e70cfc
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2024-01-07 08:05:30 +0530

    Time: 535 ms (57.50%), Space: 154.1 MB (65.83%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-int numberOfArithmeticSlices(vector<int>& A) {
-    if(A.empty()) return 0;
-    vector<unordered_map<int,int>> dp(A.size());//[index, [difference, count]]
-    
-    int res = 0;
-    for(int i = 0; i < A.size(); ++i){
-        for(int j = 0; j < i; ++j){
-            if((long)A[i] - (long)A[j] > INT_MAX || (long)A[i] - (long)A[j] < INT_MIN) continue;// do not ignore this step, it will help you save time & space to pass OJ.
-            int dif = A[i] - A[j];
-            dp[i][dif] += 1;
-            if(dp[j].find(dif) != dp[j].end()){
-                dp[i][dif] += dp[j][dif];
-                res += dp[j][dif];
+    int numberOfArithmeticSlices(vector<int>& A, int res = 0) {
+        unordered_map<long, int> m[max(1, (int)A.size())];
+        for (int i = 0; i < A.size(); ++i)
+            for (int j = 0; j < i; ++j) {
+                auto it = m[j].find((long)A[i] - A[j]);
+                int cnt = it == end(m[j]) ? 0 : it->second;
+                m[i][(long)A[i] - A[j]] += cnt + 1;
+                res += cnt;
             }
-        }
+        return res;
     }
-    return res;
-}
 };
