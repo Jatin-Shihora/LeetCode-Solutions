02514e8f6cfbc7e9ebc32eb774e48841a8a5fee6
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2024-03-28 16:39:06 +0530

    Time: 35 ms (95.63%), Space: 36.6 MB (97.61%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    public: int subarraysWithKDistinct(vector < int > & nums, int k) {
+        // Array to store the count of distinct values encountered
+        vector <int> distinctCount(nums.size() + 1, 0);
+
+        int totalCount = 0;
+        int left = 0;
+        int right = 0;
+        int currCount = 0;
+
+        while (right < nums.size()) {
+            // Increment the count of the current element in the window
+            if (++distinctCount[nums[right++]] == 1) {
+                // If encountering a new distinct element, decrement K
+                k--;
+            }
+
+            // If K becomes negative, adjust the window from the left
+            if (k < 0) {
+                // Move the left pointer until the count of distinct elements becomes valid again
+                --distinctCount[nums[left++]];
+                k++;
+                currCount = 0;
+            }
+
+            // If K becomes zero, calculate subarrays
+            if (k == 0) {
+                // While the count of left remains greater than 1, keep shrinking the window from the left
+                while (distinctCount[nums[left]] > 1) {
+                    --distinctCount[nums[left++]];
+                    currCount++;
+                }
+                // Add the count of subarrays with k distinct elements to the total count
+                totalCount += (currCount + 1);
+            }
+        }
+        return totalCount;
+    }
+};
