81bb6d645749df3c3af4a54e8d74e5e2eebb2c1a
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2024-06-03 08:16:30 +0530

    Time: 22 ms (96%), Space: 45.3 MB (62.79%) - LeetHub

---
+++ 0876-hand-of-straights/0876-hand-of-straights.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        // Map to store the count of each card value
+        Map<Integer, Integer> cardCount = new HashMap<>();
+        for (int card : hand) {
+            cardCount.put(card, cardCount.getOrDefault(card, 0) + 1);
+        }
+        
+        for (int card : hand) {
+            int startCard = card;
+            // Find the start of the potential straight sequence
+            while (cardCount.getOrDefault(startCard - 1, 0) > 0) {
+                startCard--;
+            }
+            
+            // Process the sequence starting from startCard
+            while (startCard <= card) {
+                while (cardCount.getOrDefault(startCard, 0) > 0) {
+                    // Check if we can form a consecutive sequence of groupSize cards
+                    for (int nextCard = startCard; nextCard < startCard + groupSize; nextCard++) {
+                        if (cardCount.getOrDefault(nextCard, 0) == 0) {
+                            return false;
+                        }
+                        cardCount.put(nextCard, cardCount.get(nextCard) - 1);
+                    }
+                }
+                startCard++;
+            }
+        }
+        
+        return true;
+    }
+}
