d61b69fe7eb4401f9f4fba513f4f0e974dadd75f
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2024-06-15 07:36:42 +0530

    Time: 500 ms (26.79%), Space: 35.6 MB (26.79%) - LeetHub

--- 1719-put-boxes-into-the-warehouse-ii/1719-put-boxes-into-the-warehouse-ii.py
+++ 1719-put-boxes-into-the-warehouse-ii/1719-put-boxes-into-the-warehouse-ii.py
@@ -8,31 +8,33 @@ class Solution:
 
         # Preprocess the height of the warehouse rooms to
         # get usable heights from the left end
+
         for i in range(n):
             minHeight = min(minHeight, warehouse[i])
             effectiveHeights[i] = minHeight
-
         minHeight = float("inf")
         # Update the effective heights considering the right end
+
         for i in range(n - 1, -1, -1):
             minHeight = min(minHeight, warehouse[i])
             effectiveHeights[i] = max(effectiveHeights[i], minHeight)
-
         # Sort the effective heights of the warehouse rooms
+
         effectiveHeights.sort()
 
         # Sort the boxes by height
+
         boxes.sort()
 
         count = 0
         boxIndex = 0
         # Try to place each box in the warehouse from
         # the smallest room to the largest
+
         for i in range(n):
-            if (boxIndex < len(boxes) and
-                boxes[boxIndex] <= effectiveHeights[i]):
+            if boxIndex < len(boxes) and boxes[boxIndex] <= effectiveHeights[i]:
                 # Place the box and move to the next one
+
                 count += 1
                 boxIndex += 1
-
         return count
