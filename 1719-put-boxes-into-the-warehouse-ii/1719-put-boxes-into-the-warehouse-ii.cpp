4de8bdd025ab61037bd9c3e3f12ada985337f781
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2024-06-15 07:24:55 +0530

    Time: 90 ms (100%), Space: 63.8 MB (97.14%) - LeetHub

--- 1719-put-boxes-into-the-warehouse-ii/1719-put-boxes-into-the-warehouse-ii.cpp
+++ 1719-put-boxes-into-the-warehouse-ii/1719-put-boxes-into-the-warehouse-ii.cpp
@@ -1,5 +1,5 @@
 class Solution {
-   public:
+public:
     int maxBoxesInWarehouse(vector<int>& boxes, vector<int>& warehouse) {
         int warehouseSize = warehouse.size();
 
@@ -13,12 +13,12 @@ class Solution {
 
         // Iterate through the boxes from largest to smallest
         while (leftIndex <= rightIndex && boxIndex >= 0) {
-            // Check if the current box can fit in the 
+            // Check if the current box can fit in the
             // leftmost available room
             if (boxes[boxIndex] <= warehouse[leftIndex]) {
                 boxCount++;
                 leftIndex++;
-                // Check if the current box can fit in the 
+                // Check if the current box can fit in the
                 // rightmost available room
             } else if (boxes[boxIndex] <= warehouse[rightIndex]) {
                 boxCount++;
