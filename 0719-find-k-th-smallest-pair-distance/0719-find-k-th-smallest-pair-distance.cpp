e0de046388c1aeca4a3a863c8c4cf98f729ed181
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2024-08-08 08:49:01 +0530

    Time: 536 ms (6.88%), Space: 25.9 MB (5.68%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,20 +1,39 @@
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
-    int smallestDistancePair(vector<int>& nums, int k) {
-        int len = nums.size();
-        int Max = *max_element(nums.begin(), nums.end());
-        vector<int> bucket(Max+1,0);
-        for(int i = 0; i < len; i++){
-            for(int j = i+1; j < len; j++){
-                ++bucket[abs(nums[i]-nums[j])];
+    int smallestDistancePair(std::vector<int>& nums, int k) {
+        // Get the size of the input array
+        int arraySize = nums.size();
+        
+        // Find the maximum element in the array
+        int maxElement = *std::max_element(nums.begin(), nums.end());
+        
+        // Initialize a bucket array to store counts of each distance
+        std::vector<int> distanceBucket(maxElement + 1, 0);
+
+        // Populate the bucket array with counts of each distance
+        for (int i = 0; i < arraySize; ++i) {
+            for (int j = i + 1; j < arraySize; ++j) {
+                // Compute the distance between nums[i] and nums[j]
+                int distance = std::abs(nums[i] - nums[j]);
+                
+                // Increment the count for this distance in the bucket
+                ++distanceBucket[distance];
             }
         }
-        for(int i = 0; i <= Max; i++){
-            k -= bucket[i];
-            if(k<=0){
-                return i;
+
+        // Find the k-th smallest distance
+        for (int dist = 0; dist <= maxElement; ++dist) {
+            // Reduce k by the number of pairs with the current distance
+            k -= distanceBucket[dist];
+            
+            // If k is less than or equal to 0, return the current distance
+            if (k <= 0) {
+                return dist;
             }
         }
-        return -1;
+        return -1; // Return -1 if no distance found, should not reach here
     }
 };
