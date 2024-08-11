1060db4fd869696c87ac68c402a98b3c8ba59a47
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2024-08-11 23:37:03 +0530

    Time: 0 ms (100%), Space: 41.1 MB (97.81%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -1,39 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        Deque<TreeNode> stack = new ArrayDeque<>();
-        TreeNode cur = root, lastVisited = null;
-        
-        while (!stack.isEmpty() || cur != null) {
-            if (cur != null) {
-                stack.addLast(cur);
-                cur = cur.left;
-            } else {
-                TreeNode peek = stack.peekLast();
-                if (peek.right != null && peek.right != lastVisited) {
-                    cur = peek.right;
-                } else {
-                    res.add(peek.val);
-                    lastVisited = stack.removeLast();
-                }
-            }
-        }
-        
-        return res;
-    }
-}
+
+   public List<Integer> postorderTraversal(TreeNode root) {
+		List<Integer> out = new ArrayList<Integer>();
+		if(root==null)
+			return out;
+		TreeNode pre=null;
+		Stack<TreeNode> s = new Stack();      
+		while(root!=null || !s.empty()){
+			if(root!=null){				
+				s.push(root);
+				root = root.left;
+			}
+			else{
+				root = s.peek();
+				if(root.right==null || root.right==pre){
+				    out.add(root.val);
+				    s.pop();
+				    pre=root;
+				    root = null;
+				}
+				else
+				    root = root.right;
+			}
+		}
+		return out;
+	}
+};
