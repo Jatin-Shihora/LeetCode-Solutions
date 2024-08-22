1eb717e394f3569b7beab3d6393a8dc8b0bdd21b
Jatin-Shihora <75017563+Jatin-Shihora@users.noreply.github.com>
2024-08-22 23:28:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int result = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            int accu = 1;
+            for (int j = i; j < nums.length; j++) {
+                accu *= nums[j];
+                result = Math.max(result, accu);
+            }
+        }
+
+        return result;
+    }
